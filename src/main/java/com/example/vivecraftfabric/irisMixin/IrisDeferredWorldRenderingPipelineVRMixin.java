37a9f08db42e596ded2b4f1e07e8618cbaef00cd
ferriarnus <61201275+ferriarnus@users.noreply.github.com>
2022-08-08 02:30:29 +0200

    fix iris to use vr render target

---
+++ src/main/java/com/example/vivecraftfabric/irisMixin/IrisDeferredWorldRenderingPipelineVRMixin.java
@@ -0,0 +1,18 @@
+package com.example.vivecraftfabric.irisMixin;
+
+import com.example.vivecraftfabric.DataHolder;
+import com.mojang.blaze3d.pipeline.RenderTarget;
+import net.coderbot.iris.pipeline.DeferredWorldRenderingPipeline;
+import net.minecraft.client.Minecraft;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(DeferredWorldRenderingPipeline.class)
+public class IrisDeferredWorldRenderingPipelineVRMixin {
+
+    @Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/Minecraft;getMainRenderTarget()Lcom/mojang/blaze3d/pipeline/RenderTarget;"), method = "<init>")
+    public RenderTarget rendertarget(Minecraft instance) {
+        return DataHolder.getInstance().vrRenderer.framebufferVrRender;
+    }
+}
---
+++ src/main/java/com/example/vivecraftfabric/irisMixin/IrisNewWorldRenderingPipelineVRMixin.java
@@ -0,0 +1,18 @@
+package com.example.vivecraftfabric.irisMixin;
+
+import com.example.vivecraftfabric.DataHolder;
+import com.mojang.blaze3d.pipeline.RenderTarget;
+import net.coderbot.iris.pipeline.newshader.NewWorldRenderingPipeline;
+import net.minecraft.client.Minecraft;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(NewWorldRenderingPipeline.class)
+public class IrisNewWorldRenderingPipelineVRMixin {
+
+    @Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/Minecraft;getMainRenderTarget()Lcom/mojang/blaze3d/pipeline/RenderTarget;"), method = "<init>")
+    public RenderTarget rendertarget(Minecraft instance) {
+        return DataHolder.getInstance().vrRenderer.framebufferVrRender;
+    }
+}
