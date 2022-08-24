0a5756d7ae460135f351ba47432b96c4e0cbc475
fayer3 <bayer.florian@aon.at>
2022-08-25 00:39:05 +0200

    fix HRTF Button

--- src/main/java/org/vivecraft/mixin/client/gui/screens/SoundOptionsScreenVRMixin.java
+++ src/main/java/org/vivecraft/mixin/client/gui/screens/SoundOptionsScreenVRMixin.java
@@ -7,8 +7,7 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TextComponent;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.vivecraft.DataHolder;
 import org.vivecraft.settings.VRSettings;
 
@@ -18,15 +17,17 @@ public class SoundOptionsScreenVRMixin extends Screen {
         super(component);
     }
 
-    @Inject(method = "init", at = @At("TAIL"))
-    private void addHRTF(CallbackInfo ci) {
+    @ModifyVariable(method = "init", at = @At(value = "LOAD", ordinal = 4), ordinal = 2)
+    private int addHRTF(int k) {
         int i = this.height / 6 - 12;
         // TODO: Remove in 1.19
-        this.addRenderableWidget(new Button(this.width / 2 - 155 + i % 2 * 160, this.height / 6 - 12 + 24 * (i >> 1), 150, 20, new TextComponent(DataHolder.getInstance().vrSettings.getButtonDisplayString(VRSettings.VrOptions.HRTF_SELECTION)), (p_213104_1_) ->
+        this.addRenderableWidget(new Button(this.width / 2 - 155 + k % 2 * 160, i + 22 * (k >> 1), 150, 20, new TextComponent(DataHolder.getInstance().vrSettings.getButtonDisplayString(VRSettings.VrOptions.HRTF_SELECTION)), (p_213104_1_) ->
         {
             this.clearWidgets();
             DataHolder.getInstance().vrSettings.setOptionValue(VRSettings.VrOptions.HRTF_SELECTION);
             DataHolder.getInstance().vrSettings.saveOptions();
+            this.init();
         }));
+        return k;
     }
 }
