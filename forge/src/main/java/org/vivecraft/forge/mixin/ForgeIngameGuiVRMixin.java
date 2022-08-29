5a348688ea0e11bfcadb1d6acb84cfe24ff68b4c
ferriarnus <61201275+ferriarnus@users.noreply.github.com>
2022-08-30 01:43:51 +0200

    fix forge vignette

---
+++ forge/src/main/java/org/vivecraft/forge/mixin/ForgeIngameGuiVRMixin.java
@@ -0,0 +1,15 @@
+package org.vivecraft.forge.mixin;
+
+import net.minecraftforge.client.gui.ForgeIngameGui;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(ForgeIngameGui.class)
+public abstract class ForgeIngameGuiVRMixin {
+
+    @Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/Minecraft;useFancyGraphics()Z"), method = "lambda$static$0")
+    private static boolean noVignette() {
+        return false;
+    }
+}
