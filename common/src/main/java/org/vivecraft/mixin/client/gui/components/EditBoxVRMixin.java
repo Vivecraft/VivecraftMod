95ce4dfa8eee52ef9b5b45ad1a6947b5b143fb6c
ferriarnus <61201275+ferriarnus@users.noreply.github.com>
2022-09-04 23:24:07 +0200

    Update EditBoxVRMixin.java

--- common/src/main/java/org/vivecraft/mixin/client/gui/components/EditBoxVRMixin.java
+++ common/src/main/java/org/vivecraft/mixin/client/gui/components/EditBoxVRMixin.java
@@ -4,6 +4,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import net.minecraft.client.gui.components.AbstractWidget;
@@ -13,21 +14,18 @@ import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(EditBox.class)
 public abstract class EditBoxVRMixin extends AbstractWidget{
-	
+
+	@Shadow
+	private boolean canLoseFocus;
+
 	public EditBoxVRMixin(int p_93629_, int p_93630_, int p_93631_, int p_93632_, Component p_93633_) {
 		super(p_93629_, p_93630_, p_93631_, p_93632_, p_93633_);
 	}
-	
-	@Shadow
-	public abstract void setFocus(boolean p_94179_);
 
 	//TODO test
-	@Inject(at = @At(value = "FIELD", target = "Lnet/minecraft/client/gui/components/EditBox;canLoseFocus:Z"), method = "Lnet/minecraft/client/gui/components/EditBox;mouseClicked(DDI)Z", locals = LocalCapture.CAPTURE_FAILHARD)
-	public void focus(double d, double e, int i, CallbackInfoReturnable<Boolean> cir, int f, boolean bl) {
-		if (!this.isFocused())
-        {
-            this.setFocus(bl);
-        }
+	@Redirect(at = @At(value = "FIELD", target = "Lnet/minecraft/client/gui/components/EditBox;canLoseFocus:Z"), method = "Lnet/minecraft/client/gui/components/EditBox;mouseClicked(DDI)Z")
+	public boolean focus(EditBox instance) {
+		return  canLoseFocus || !this.isFocused();
 	}
 
 }
