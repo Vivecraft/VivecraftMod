0a63056bce2e259ab44cadf8026abbb097c0bd70
fayer3 <bayer.florian@aon.at>
2022-09-29 19:12:33 +0200

    combine some only slightly differing forge/fabric mixins into the common source, with mixin groups move forge model loading to forge events backport forge overlay handling from 1.19

--- forge/src/main/java/org/vivecraft/forge/mixin/ForgeIngameGuiVRMixin.java
+++ forge/src/main/java/org/vivecraft/forge/mixin/ForgeIngameGuiVRMixin.java
@@ -1,36 +1,20 @@
 package org.vivecraft.forge.mixin;
 
+import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraftforge.client.gui.ForgeIngameGui;
+import net.minecraftforge.client.gui.IIngameOverlay;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ForgeIngameGui.class)
 public abstract class ForgeIngameGuiVRMixin {
 
-    @Inject(at = @At("HEAD"), method = "lambda$static$0", remap = false, cancellable = true)
-    private static void noVignette(CallbackInfo ci) {
-        ci.cancel();
-    }
-
-    @Inject(at = @At("HEAD"), method = "lambda$static$1", remap = false, cancellable = true)
-    private static void noSpyglass(CallbackInfo ci) {
-        ci.cancel();
-    }
-
-    @Inject(at = @At("HEAD"), method = "lambda$static$2", remap = false, cancellable = true)
-    private static void noHelmet(CallbackInfo ci) {
-        ci.cancel();
-    }
-
-    @Inject(at = @At("HEAD"), method = "lambda$static$3", remap = false, cancellable = true)
-    private static void noFreeze(CallbackInfo ci) {
-        ci.cancel();
-    }
-
-    @Inject(at = @At("HEAD"), method = "lambda$static$4", remap = false, cancellable = true)
-    private static void noPortal(CallbackInfo ci) {
-        ci.cancel();
+    @Inject(method = "pre(Lnet/minecraftforge/client/gui/IIngameOverlay;Lcom/mojang/blaze3d/vertex/PoseStack;)Z", at = @At("HEAD"), remap = false, cancellable = true)
+    private void noStuff(IIngameOverlay overlay, PoseStack poseStack, CallbackInfoReturnable<Boolean> info) {
+        if (overlay == ForgeIngameGui.VIGNETTE_ELEMENT || overlay == ForgeIngameGui.SPYGLASS_ELEMENT || overlay == ForgeIngameGui.HELMET_ELEMENT || overlay == ForgeIngameGui.FROSTBITE_ELEMENT || overlay == ForgeIngameGui.PORTAL_ELEMENT) {
+            info.setReturnValue(true);
+        }
     }
 }
