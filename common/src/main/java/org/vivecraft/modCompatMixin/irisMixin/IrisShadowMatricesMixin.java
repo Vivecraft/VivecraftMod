946825993afcbb724a7ea7f32ecc87d0f439eab4
fayer3 <bayer.florian@aon.at>
2022-10-16 01:47:17 +0200

    iris 1.5.0 compat fix iris in dev, by switching mixin method targets to Descs

--- common/src/main/java/org/vivecraft/modCompatMixin/irisMixin/IrisShadowMatricesMixin.java
+++ common/src/main/java/org/vivecraft/modCompatMixin/irisMixin/IrisShadowMatricesMixin.java
@@ -11,20 +11,20 @@ import org.vivecraft.ClientDataHolder;
 import org.vivecraft.render.RenderPass;
 
 @Pseudo
-@Mixin(ShadowMatrices.class)
+@Mixin(targets = {"net.coderbot.iris.shadow.ShadowMatrices", "net.coderbot.iris.shadows.ShadowMatrices"})
 public class IrisShadowMatricesMixin {
 
     private static float cachedShadowIntervalSize;
     private static Vec3 leftPass;
     private static Vec3 currentPass;
 
-    @Inject(method = "snapModelViewToGrid", at = @At("HEAD"))
+    @Inject(method = "snapModelViewToGrid", at = @At("HEAD"), remap = false)
     private static void cacheInterval(Matrix4f target, float shadowIntervalSize, double cameraX, double cameraY, double cameraZ, CallbackInfo ci){
         cachedShadowIntervalSize = shadowIntervalSize;
     }
 
     // offset camera pos, to be in the equal grid as left eye, but with correct offset
-    @ModifyVariable( method = "snapModelViewToGrid", at = @At(value = "STORE"), ordinal  = 1)
+    @ModifyVariable( method = "snapModelViewToGrid", at = @At(value = "STORE"), ordinal  = 1, remap = false)
     private static float modifyOffsetX(float original){
         currentPass = ClientDataHolder.getInstance().vrPlayer.getVRDataWorld().getEye(ClientDataHolder.getInstance().currentPass).getPosition();
         if (ClientDataHolder.getInstance().currentPass == RenderPass.LEFT) {
@@ -32,11 +32,11 @@ public class IrisShadowMatricesMixin {
         }
         return (float) (leftPass.x % cachedShadowIntervalSize - (leftPass.x - currentPass.x));
     }
-    @ModifyVariable( method = "snapModelViewToGrid", at = @At(value = "STORE"), ordinal  = 2)
+    @ModifyVariable( method = "snapModelViewToGrid", at = @At(value = "STORE"), ordinal  = 2, remap = false)
     private static float modifyOffsetY(float original){
         return (float) (leftPass.y % cachedShadowIntervalSize - (leftPass.y - currentPass.y));
     }
-    @ModifyVariable( method = "snapModelViewToGrid", at = @At(value = "STORE"), ordinal  = 3)
+    @ModifyVariable( method = "snapModelViewToGrid", at = @At(value = "STORE"), ordinal  = 3, remap = false)
     private static float modifyOffsetZ(float original){
         return (float) (leftPass.z % cachedShadowIntervalSize - (leftPass.z - currentPass.z));
     }
