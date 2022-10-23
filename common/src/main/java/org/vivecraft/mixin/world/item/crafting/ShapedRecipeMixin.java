65b0edbea303f6f7fbdeaf48831e6a6a90d95c8c
fayer3 <bayer.florian@aon.at>
2022-10-24 01:59:27 +0200

    change crafting, to use vanilla minecraft:crafting_shaped, with custom additions

---
+++ common/src/main/java/org/vivecraft/mixin/world/item/crafting/ShapedRecipeMixin.java
@@ -0,0 +1,50 @@
+package org.vivecraft.mixin.world.item.crafting;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
+import com.google.gson.JsonSyntaxException;
+import net.minecraft.core.Registry;
+import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.GsonHelper;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.ShapedRecipe;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+@Mixin(ShapedRecipe.class)
+public class ShapedRecipeMixin {
+    @Inject(method = "itemFromJson", at = @At(value = "INVOKE", target = "Lnet/minecraft/core/DefaultedRegistry;getOptional(Lnet/minecraft/resources/ResourceLocation;)Ljava/util/Optional;", shift = At.Shift.BEFORE), locals = LocalCapture.CAPTURE_FAILHARD, cancellable = true)
+    private static void vivecraftItems(JsonObject jsonObject, CallbackInfoReturnable<Item> cir, String resourceLocation){
+        if (resourceLocation.startsWith("vivecraft")) {
+            String vanillaItem = GsonHelper.getAsString(jsonObject, "vanillaitem");
+            cir.setReturnValue(Registry.ITEM.getOptional(new ResourceLocation(vanillaItem)).orElseThrow(() -> new JsonSyntaxException("Unknown item '" + vanillaItem + "'")));
+
+        }
+    }
+
+    @Inject(method = "itemStackFromJson", at = @At(value = "INVOKE", target = "Lcom/google/gson/JsonObject;has(Ljava/lang/String;)Z", shift = At.Shift.BEFORE, remap = false), locals = LocalCapture.CAPTURE_FAILHARD, cancellable = true)
+    private static void vivecraftItems(JsonObject jsonObject, CallbackInfoReturnable<ItemStack> cir, Item vanillaItem){
+        if (GsonHelper.getAsString(jsonObject, "item").startsWith("vivecraft")) {
+            if (jsonObject.has("data")) {
+                throw new JsonParseException("Disallowed data tag found");
+            } else {
+                int i = GsonHelper.getAsInt(jsonObject, "count", 1);
+                if (i < 1) {
+                    throw new JsonSyntaxException("Invalid output count: " + i);
+                } else {
+                    ItemStack itemStack = new ItemStack(vanillaItem, i);
+                    itemStack.setHoverName(new TranslatableComponent(jsonObject.get("name").getAsString()));
+                    itemStack.getOrCreateTag().putBoolean("Unbreakable", GsonHelper.getAsBoolean(jsonObject, "unbreakable", false));
+                    itemStack.getOrCreateTag().putInt("HideFlags", GsonHelper.getAsInt(jsonObject, "hideflags", 0));
+                    cir.setReturnValue(itemStack);
+                }
+            }
+        }
+    }
+
+}
