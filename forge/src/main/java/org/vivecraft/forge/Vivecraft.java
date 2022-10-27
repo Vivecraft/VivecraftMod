939636f109e9d51562bb2bf9cccc2b62045cba81
fayer3 <bayer.florian@aon.at>
2022-10-28 00:09:56 +0200

    fix forge server mod making the mod required on the client

--- forge/src/main/java/org/vivecraft/forge/Vivecraft.java
+++ forge/src/main/java/org/vivecraft/forge/Vivecraft.java
@@ -1,12 +1,18 @@
 package org.vivecraft.forge;
 
+import net.minecraftforge.fml.IExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.network.NetworkConstants;
 
 @Mod(Vivecraft.MODID)
 public class Vivecraft {
     public static final String MODID = "vivecraft";
 
     public Vivecraft() {
-
+        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, ()-> new IExtensionPoint.DisplayTest(
+                ()->NetworkConstants.IGNORESERVERONLY, // only needed on server, client is optional
+                (s,b)->true // any version is good
+        ));
     }
 }
