2e05716393c3316a4fcf23f048e4e0d79fb2be02
fayer3 <bayer.florian@aon.at>
2022-10-28 23:47:57 +0200

    change HRTF option, to use the vanilla button, and vanilla activation

--- common/src/main/java/org/vivecraft/mixin/blaze3d/audio/LibraryVRMixin.java
+++ common/src/main/java/org/vivecraft/mixin/blaze3d/audio/LibraryVRMixin.java
@@ -1,25 +1,13 @@
 package org.vivecraft.mixin.blaze3d.audio;
 
 import com.mojang.blaze3d.audio.Library;
-import org.lwjgl.BufferUtils;
-import org.lwjgl.openal.AL10;
-import org.lwjgl.openal.ALC10;
-import org.lwjgl.openal.ALCCapabilities;
-import org.lwjgl.openal.ALCapabilities;
-import org.lwjgl.openal.SOFTHRTF;
 import org.slf4j.Logger;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.vivecraft.ClientDataHolder;
-
-import java.nio.Buffer;
-import java.nio.IntBuffer;
-import java.util.Objects;
 
 @Mixin(Library.class)
 public class LibraryVRMixin {
@@ -27,109 +15,9 @@ public class LibraryVRMixin {
     @Final
     static Logger LOGGER;
 
-    @Shadow
-    private long currentDevice;
-
-    private static boolean checkALError(String string) {
-        int i = AL10.alGetError();
-        if (i != 0) {
-            LOGGER.error("{}: {}", string, alErrorToString(i));
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    private static String alErrorToString(int i) {
-        return switch (i) {
-            case 40961 -> "Invalid name parameter.";
-            case 40962 -> "Invalid enumerated parameter value.";
-            case 40963 -> "Invalid parameter parameter value.";
-            case 40964 -> "Invalid operation.";
-            case 40965 -> "Unable to allocate memory.";
-            default -> "An unrecognized error occurred.";
-        };
-    }
-
-    //@Inject(method = "init", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/audio/OpenAlUtil;checkALError(Ljava/lang/String;)Z", ordinal = 0, shift = At.Shift.AFTER), locals = LocalCapture.CAPTURE_FAILHARD)
-    private void setHRTF(String string, CallbackInfo ci, ALCCapabilities aLCCapabilities, int i, int j, int k, ALCapabilities aLCapabilities) {
-        ClientDataHolder.hrtfList.clear();
-
-        if (aLCCapabilities.ALC_SOFT_HRTF) {
-            int l = ALC10.alcGetInteger(this.currentDevice, 6548);
-
-            if (l > 0) {
-                LOGGER.info("Available HRTFs:");
-
-                for (int i1 = 0; i1 < l; i1++) {
-                    String s = Objects.requireNonNull(SOFTHRTF.alcGetStringiSOFT(this.currentDevice, 6549, i1));
-                    ClientDataHolder.hrtfList.add(s);
-                    LOGGER.info("{}: {}", i1, s);
-                }
-
-                int k1 = ClientDataHolder.getInstance().vrSettings.hrtfSelection;
-                int l1;
-
-                if (k1 == -1) {
-                    l1 = 0;
-                } else {
-                    l1 = 1;
-                }
-
-                IntBuffer intbuffer = BufferUtils.createIntBuffer(10).put(6546).put(l1);
-
-                if (k1 != -1) {
-                    if (k1 > 0 && k1 <= ClientDataHolder.hrtfList.size()) {
-                        LOGGER.info("Using HRTF: {}", ClientDataHolder.hrtfList.get(k1 - 1));
-                        intbuffer.put(6550).put(k1 - 1);
-                    } else {
-                        if (k1 > ClientDataHolder.hrtfList.size()) {
-                            LOGGER.warn("Invalid HRTF index: {}", k1);
-                        }
-
-                        LOGGER.info("Using default HRTF");
-                    }
-                } else {
-                    LOGGER.info("Disabling HRTF");
-                }
-
-                ((Buffer) intbuffer.put(0)).flip();
-                SOFTHRTF.alcResetDeviceSOFT(this.currentDevice, intbuffer);
-
-                if (!checkALError("HRTF initialization")) {
-                    LOGGER.info("HRTF initialized.");
-                    int j1 = ALC10.alcGetInteger(this.currentDevice, 6547);
-
-                    switch (j1) {
-                        case 0:
-                            LOGGER.info("HRTF status: disabled");
-                            break;
-
-                        case 1:
-                            LOGGER.info("HRTF status: enabled");
-                            break;
-
-                        case 2:
-                            LOGGER.info("HRTF status: denied");
-                            break;
-
-                        case 3:
-                            LOGGER.info("HRTF status: required");
-                            break;
-
-                        case 4:
-                            LOGGER.info("HRTF status: headphones detected");
-                            break;
-
-                        case 5:
-                            LOGGER.info("HRTF status: unsupported format");
-                    }
-                }
-            } else {
-                LOGGER.warn("No HRTFs found.");
-            }
-        } else {
-            LOGGER.warn("ALC_SOFT_HRTF is not supported.");
-        }
+    @ModifyVariable(method = "init", at = @At("HEAD"), argsOnly = true)
+    private boolean shouldDoHRTF(boolean vanillaHRTF) {
+        LOGGER.info("enabling HRTF: {}", ClientDataHolder.getInstance().vrSettings.hrtfSelection >= 0);
+        return ClientDataHolder.getInstance().vrSettings.hrtfSelection >= 0;
     }
 }
--- common/src/main/java/org/vivecraft/mixin/client/gui/screens/SoundOptionsScreenVRMixin.java
+++ common/src/main/java/org/vivecraft/mixin/client/gui/screens/SoundOptionsScreenVRMixin.java
@@ -1,15 +1,25 @@
 package org.vivecraft.mixin.client.gui.screens;
 
-import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.OptionInstance;
+import net.minecraft.client.Options;
+import net.minecraft.client.gui.components.AbstractWidget;
+import net.minecraft.client.gui.components.CycleButton;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.SoundOptionsScreen;
+import net.minecraft.client.resources.sounds.SimpleSoundInstance;
+import net.minecraft.client.sounds.SoundManager;
+import net.minecraft.network.chat.CommonComponents;
 import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.Component;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.util.FormattedCharSequence;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.vivecraft.ClientDataHolder;
 import org.vivecraft.settings.VRSettings;
+
+import java.util.List;
 
 @Mixin(SoundOptionsScreen.class)
 public class SoundOptionsScreenVRMixin extends Screen {
@@ -17,17 +27,25 @@ public class SoundOptionsScreenVRMixin extends Screen {
         super(component);
     }
 
-    @ModifyVariable(method = "init", at = @At(value = "LOAD", ordinal = 4), ordinal = 2)
-    private int addHRTF(int k) {
-        int i = this.height / 6 - 12;
-        // TODO: Remove in 1.19
-        this.addRenderableWidget(new Button(this.width / 2 - 155 + k % 2 * 160, i + 22 * (k >> 1), 150, 20, Component.literal(ClientDataHolder.getInstance().vrSettings.getButtonDisplayString(VRSettings.VrOptions.HRTF_SELECTION)), (p_213104_1_) ->
-        {
-            this.clearWidgets();
-            ClientDataHolder.getInstance().vrSettings.setOptionValue(VRSettings.VrOptions.HRTF_SELECTION);
-            ClientDataHolder.getInstance().vrSettings.saveOptions();
-            this.init();
-        }));
-        return k;
+    // replace vanilla button with ours, so directional audio is on by default, when playing VR
+    @Redirect(method = "init", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/OptionInstance;createButton(Lnet/minecraft/client/Options;III)Lnet/minecraft/client/gui/components/AbstractWidget;", ordinal = 2))
+    private AbstractWidget redirectHRTFButton(OptionInstance instance, Options options, int i, int j, int k){
+        return CycleButton.builder((bool) -> (boolean)bool ? CommonComponents.OPTION_ON : CommonComponents.OPTION_OFF)
+            .withValues(false, true)
+            .withInitialValue(ClientDataHolder.getInstance().vrSettings.hrtfSelection >= 0)
+            .withTooltip(obj -> {
+                List<FormattedCharSequence> tooltipON = minecraft.font.split(Component.translatable("options.directionalAudio.on.tooltip"), 200);
+                List<FormattedCharSequence> tooltipOFF = minecraft.font.split(Component.translatable("options.directionalAudio.off.tooltip"), 200);
+                return ClientDataHolder.getInstance().vrSettings.hrtfSelection >= 0 ? tooltipON : tooltipOFF;
+            })
+            .create(i, j, k, 20, Component.translatable("options.directionalAudio"), (cycleButton, newValue) -> {
+                ClientDataHolder.getInstance().vrSettings.hrtfSelection = (boolean)newValue ? 0 : -1;
+                ClientDataHolder.getInstance().vrSettings.setOptionValue(VRSettings.VrOptions.HRTF_SELECTION);
+                ClientDataHolder.getInstance().vrSettings.saveOptions();
+
+                SoundManager soundManager = Minecraft.getInstance().getSoundManager();
+                soundManager.reload();
+                soundManager.play(SimpleSoundInstance.forUI(SoundEvents.UI_BUTTON_CLICK, 1.0F));
+            });
     }
 }
