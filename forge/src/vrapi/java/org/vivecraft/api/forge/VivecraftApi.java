b68b02be6f26a87413a8d562ff49ba8a2fb7403b
ferriarnus <61201275+ferriarnus@users.noreply.github.com>
2022-11-07 11:58:47 +0100

    rename to vrapi

---
+++ forge/src/vrapi/java/org/vivecraft/api/forge/VivecraftApi.java
@@ -0,0 +1,9 @@
+package org.vivecraft.api.forge;
+
+@Mod(VivecraftApi.MODID)
+public class VivecraftApi {
+    public static final String MODID = "vivecraftapi";
+
+    public VivecraftApi() {
+    }
+}
