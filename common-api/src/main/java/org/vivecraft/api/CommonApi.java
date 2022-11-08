96a7d6e15fa6163e27656a3c08a92575cefbb025
ferriarnus <61201275+ferriarnus@users.noreply.github.com>
2022-11-08 11:49:47 +0100

    yafr

---
+++ common-api/src/main/java/org/vivecraft/api/CommonApi.java
@@ -0,0 +1,4 @@
+package org.vivecraft.api;
+
+public class CommonApi {
+}
---
+++ forge-api/src/main/java/org/vivecraft/api/VivecraftApi.java
@@ -0,0 +1,13 @@
+package org.vivecraft.api;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(VivecraftApi.MODID)
+public class VivecraftApi {
+
+    public static final String MODID = "vivecraftapi";
+
+    public VivecraftApi() {
+
+    }
+}
