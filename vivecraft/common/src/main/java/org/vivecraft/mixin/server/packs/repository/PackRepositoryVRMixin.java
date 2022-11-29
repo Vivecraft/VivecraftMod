7b02ba0db498854bf9db54b1bd75b6dec7066cb1
ferriarnus <61201275+ferriarnus@users.noreply.github.com>
2022-11-29 10:43:18 +0100

    why not...

---
+++ vivecraft/common/src/main/java/org/vivecraft/mixin/server/packs/repository/PackRepositoryVRMixin.java
@@ -0,0 +1,38 @@
+package org.vivecraft.mixin.server.packs.repository;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import com.google.common.collect.Lists;
+
+import net.minecraft.server.packs.repository.Pack;
+import net.minecraft.server.packs.repository.PackRepository;
+import org.vivecraft.CommonDataHolder;
+
+@Mixin(PackRepository.class)
+public class PackRepositoryVRMixin {
+	
+	@Shadow
+	private List<Pack> selected;
+	
+	@Inject(at = @At("TAIL"), method = "reload()V")
+	public void reload(CallbackInfo info) {
+		if (!this.selected.equals(Lists.newLinkedList(this.selected))) {
+			CommonDataHolder.getInstance().resourcePacksChanged = true;
+		}
+	}
+	
+	@Inject(at = @At("TAIL"), method = "setSelected(Ljava/util/Collection;)V")
+	public void selected(Collection<String> c, CallbackInfo info) {
+		if (!this.selected.equals(Lists.newLinkedList(this.selected))) {
+			CommonDataHolder.getInstance().resourcePacksChanged = true;
+		}
+	}
+
+}
---
+++ vivecraft/fabric/src/main/java/org/vivecraft/fabric/mixin/FabricGameRendererVRMixin.java
@@ -0,0 +1,24 @@
+package org.vivecraft.fabric.mixin;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.math.Quaternion;
+import net.minecraft.client.renderer.GameRenderer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.vivecraft.ClientDataHolder;
+import org.vivecraft.extensions.GameRendererExtension;
+
+@Mixin(GameRenderer.class)
+public class FabricGameRendererVRMixin {
+
+    @Redirect(at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/vertex/PoseStack;mulPose(Lcom/mojang/math/Quaternion;)V ", ordinal = 2), method = "renderLevel(FJLcom/mojang/blaze3d/vertex/PoseStack;)V")
+    public void removeMulposeX(PoseStack s, Quaternion quaternion) {
+        return;
+    }
+
+    @Redirect(at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/vertex/PoseStack;mulPose(Lcom/mojang/math/Quaternion;)V ", ordinal = 3), method = "renderLevel(FJLcom/mojang/blaze3d/vertex/PoseStack;)V")
+    public void removeMulposeY(PoseStack s, Quaternion quaternion) {
+        ((GameRendererExtension)this).applyVRModelView(ClientDataHolder.getInstance().currentPass, s);
+    }
+}
