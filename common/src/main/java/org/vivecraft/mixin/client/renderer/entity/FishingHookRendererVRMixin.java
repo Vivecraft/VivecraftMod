b9b06486f98550471606716934eceb211b2846d5
fayer3 <fayer3@users.noreply.github.com>
2023-01-20 22:14:01 +0100

    fix fishing line on forge 1.19+, by modifying on LOAD instead of STORE

--- common/src/main/java/org/vivecraft/mixin/client/renderer/entity/FishingHookRendererVRMixin.java
+++ common/src/main/java/org/vivecraft/mixin/client/renderer/entity/FishingHookRendererVRMixin.java
@@ -2,6 +2,8 @@ package org.vivecraft.mixin.client.renderer.entity;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.entity.EntityRenderer;
+import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.world.entity.projectile.FishingHook;
 import org.spongepowered.asm.mixin.injection.*;
 import net.minecraft.client.Minecraft;
@@ -14,7 +16,12 @@ import org.vivecraft.ClientDataHolder;
 import org.vivecraft.extensions.GameRendererExtension;
 
 @Mixin(FishingHookRenderer.class)
-public class FishingHookRendererVRMixin {
+public abstract class FishingHookRendererVRMixin extends EntityRenderer<FishingHook> {
+
+    // dummy constructor
+    protected FishingHookRendererVRMixin(EntityRendererProvider.Context context) {
+        super(context);
+    }
 
     private FishingHook currentlyRenderingFishingHook;
     private Vec3 CachedHandPos;
@@ -24,32 +31,47 @@ public class FishingHookRendererVRMixin {
         currentlyRenderingFishingHook = fishingHook;
     }
 
-    @ModifyVariable(at = @At(value = "STORE", ordinal = 1),
+    @ModifyVariable(at = @At(value = "LOAD"),
             method = "render(Lnet/minecraft/world/entity/projectile/FishingHook;FFLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;I)V", index = 25)
     private double fishingLineStartX(double value) {
-        int j = 1;
-        if (currentlyRenderingFishingHook.getPlayerOwner().getMainHandItem().getItem() instanceof FishingRodItem)
-        {
-            j = 0;
+        if ((this.entityRenderDispatcher.options == null || this.entityRenderDispatcher.options.getCameraType().isFirstPerson()) && currentlyRenderingFishingHook.getPlayerOwner() == Minecraft.getInstance().player) {
+            int j = 1;
+            if (currentlyRenderingFishingHook.getPlayerOwner().getMainHandItem().getItem() instanceof FishingRodItem) {
+                j = 0;
+            }
+            Vec3 vec31 = ((GameRendererExtension) Minecraft.getInstance().gameRenderer).getControllerRenderPos(j);
+            Vec3 vec32 = ClientDataHolder.getInstance().vrPlayer.vrdata_world_render.getHand(j).getDirection();
+            CachedHandPos = vec31.add(vec32.scale(0.47 * ClientDataHolder.getInstance().vrPlayer.vrdata_world_render.worldScale));
+            return CachedHandPos.x;
+        } else {
+            return value;
         }
-        Vec3 vec31 = ((GameRendererExtension) Minecraft.getInstance().gameRenderer).getControllerRenderPos(j);
-        Vec3 vec32 = ClientDataHolder.getInstance().vrPlayer.vrdata_world_render.getHand(j).getDirection();
-        CachedHandPos = vec31.add(vec32.scale(0.47 * ClientDataHolder.getInstance().vrPlayer.vrdata_world_render.worldScale));
-        return CachedHandPos.x;
     }
-    @ModifyVariable(at = @At(value = "STORE", ordinal = 1),
+    @ModifyVariable(at = @At(value = "LOAD"),
             method = "render(Lnet/minecraft/world/entity/projectile/FishingHook;FFLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;I)V", index = 27)
     private double fishingLineStartY(double value) {
-        return CachedHandPos.y;
+        if ((this.entityRenderDispatcher.options == null || this.entityRenderDispatcher.options.getCameraType().isFirstPerson()) && currentlyRenderingFishingHook.getPlayerOwner() == Minecraft.getInstance().player) {
+            return CachedHandPos.y;
+        } else {
+            return value;
+        }
     }
-    @ModifyVariable(at = @At(value = "STORE", ordinal = 1),
+    @ModifyVariable(at = @At(value = "LOAD"),
             method = "render(Lnet/minecraft/world/entity/projectile/FishingHook;FFLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;I)V", index = 29)
     private double fishingLineStartZ(double value) {
-        return CachedHandPos.z;
+        if ((this.entityRenderDispatcher.options == null || this.entityRenderDispatcher.options.getCameraType().isFirstPerson()) && currentlyRenderingFishingHook.getPlayerOwner() == Minecraft.getInstance().player) {
+            return CachedHandPos.z;
+        } else {
+            return value;
+        }
     }
-    @ModifyVariable(at = @At(value = "STORE", ordinal = 1),
+    @ModifyVariable(at = @At(value = "LOAD"),
             method = "render(Lnet/minecraft/world/entity/projectile/FishingHook;FFLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;I)V", index = 31)
     private float fishingLineStartOffset(float value) {
-        return 0.0F;
+        if ((this.entityRenderDispatcher.options == null || this.entityRenderDispatcher.options.getCameraType().isFirstPerson()) && currentlyRenderingFishingHook.getPlayerOwner() == Minecraft.getInstance().player) {
+            return 0.0F;
+        } else {
+            return value;
+        }
     }
 }
