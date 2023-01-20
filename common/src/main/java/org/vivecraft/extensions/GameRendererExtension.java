662bf367ad2cc0925623db6faeb99fb2cba3307b
fayer3 <fayer3@users.noreply.github.com>
2023-01-20 22:13:09 +0100

    remove now unused function from GameRendererExtension

--- common/src/main/java/org/vivecraft/extensions/GameRendererExtension.java
+++ common/src/main/java/org/vivecraft/extensions/GameRendererExtension.java
@@ -44,8 +44,6 @@ public interface GameRendererExtension {
 
 	Matrix4f getThirdPassProjectionMatrix();
 
-	void drawFramebufferNEW(float f, boolean pRenderLevel, PoseStack poseStack);
-
 	void drawEyeStencil(boolean flag1);
 
 	float inBlock();
