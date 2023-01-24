9be2de3c3fbb76d9505b2c36d991a732e6016030
hammy3502 <hammy275@gmail.com>
2023-01-24 17:30:00 -0500

    ProjectileMixin Unused Imports

--- common/src/main/java/org/vivecraft/mixin/world/entity/projectile/ProjectileMixin.java
+++ common/src/main/java/org/vivecraft/mixin/world/entity/projectile/ProjectileMixin.java
@@ -1,6 +1,5 @@
 package org.vivecraft.mixin.world.entity.projectile;
 
-import net.minecraft.util.Mth;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.player.Player;
@@ -12,12 +11,7 @@ import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyArgs;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
-import org.vivecraft.api.ClientNetworkHelper;
 import org.vivecraft.api.CommonNetworkHelper;
 import org.vivecraft.api.ServerVivePlayer;
 
