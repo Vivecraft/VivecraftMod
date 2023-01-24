c63fcdcdfcd69cadde356be37e0d1e8ea39d8da0
hammy3502 <hammy275@gmail.com>
2023-01-24 17:48:40 -0500

    ProjectileMixin Remove index and cache player and controllerdir

--- common/src/main/java/org/vivecraft/mixin/world/entity/projectile/ProjectileMixin.java
+++ common/src/main/java/org/vivecraft/mixin/world/entity/projectile/ProjectileMixin.java
@@ -10,6 +10,7 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.vivecraft.api.CommonNetworkHelper;
@@ -17,6 +18,12 @@ import org.vivecraft.api.ServerVivePlayer;
 
 @Mixin(Projectile.class)
 public abstract class ProjectileMixin extends Entity {
+
+	@Unique
+	private ServerVivePlayer serverVivePlayer;
+
+	@Unique
+	private Vec3 controllerDir;
 
 	@Shadow
 	public abstract void shoot(double f2, double f, double f1, float pVelocity, float pInaccuracy);
@@ -27,41 +34,35 @@ public abstract class ProjectileMixin extends Entity {
 	}
 
 	@ModifyVariable(method = "shootFromRotation(Lnet/minecraft/world/entity/Entity;FFFFF)V",
-			at = @At("HEAD"), index = 2, ordinal = 0, argsOnly = true)
+			at = @At("HEAD"), ordinal = 0, argsOnly = true)
 	public float pX(float pXIn, Entity pProjectile) {
-		ServerVivePlayer serverviveplayer = CommonNetworkHelper.vivePlayers.get(pProjectile.getUUID());
-		if (serverviveplayer != null && serverviveplayer.isVR()) {
-			Vec3 vec3 = serverviveplayer.getControllerDir(serverviveplayer.activeHand);
-			if (((Projectile) (Object) this) instanceof AbstractArrow && !(((Projectile) (Object) this) instanceof ThrownTrident) && !serverviveplayer.isSeated() && serverviveplayer.getDraw() > 0.0F) {
-				vec3 = serverviveplayer.getControllerPos(1, (Player) pProjectile).subtract(serverviveplayer.getControllerPos(0, (Player) pProjectile)).normalize();
+		this.serverVivePlayer = CommonNetworkHelper.vivePlayers.get(pProjectile.getUUID());
+		if (this.serverVivePlayer != null && this.serverVivePlayer.isVR()) {
+			this.controllerDir = this.serverVivePlayer.getControllerDir(this.serverVivePlayer.activeHand);
+			if (((Projectile) (Object) this) instanceof AbstractArrow && !(((Projectile) (Object) this) instanceof ThrownTrident) && !this.serverVivePlayer.isSeated() && this.serverVivePlayer.getDraw() > 0.0F) {
+				this.controllerDir = this.serverVivePlayer.getControllerPos(1, (Player) pProjectile).subtract(this.serverVivePlayer.getControllerPos(0, (Player) pProjectile)).normalize();
 			}
-			return -((float) Math.toDegrees(Math.asin(vec3.y / vec3.length())));
+			return -((float) Math.toDegrees(Math.asin(this.controllerDir.y / this.controllerDir.length())));
 		}
 		return pXIn;
 	}
 
 	@ModifyVariable(method = "shootFromRotation(Lnet/minecraft/world/entity/Entity;FFFFF)V",
-			at = @At("HEAD"), index = 3, ordinal = 1, argsOnly = true)
+			at = @At("HEAD"), ordinal = 1, argsOnly = true)
 	public float pY(float pYIn, Entity pProjectile) {
-		ServerVivePlayer serverviveplayer = CommonNetworkHelper.vivePlayers.get(pProjectile.getUUID());
-		if (serverviveplayer != null && serverviveplayer.isVR()) {
-			Vec3 vec3 = serverviveplayer.getControllerDir(serverviveplayer.activeHand);
-			if (((Projectile) (Object) this) instanceof AbstractArrow && !(((Projectile) (Object) this) instanceof ThrownTrident) && !serverviveplayer.isSeated() && serverviveplayer.getDraw() > 0.0F) {
-				vec3 = serverviveplayer.getControllerPos(1, (Player) pProjectile).subtract(serverviveplayer.getControllerPos(0, (Player) pProjectile)).normalize();
-			}
-			return (float) Math.toDegrees(Math.atan2(-vec3.x, vec3.z));
+		if (this.controllerDir != null) {
+			return (float) Math.toDegrees(Math.atan2(-this.controllerDir.x, this.controllerDir.z));
 		}
 		return pYIn;
 	}
 
 	@ModifyVariable(method = "shootFromRotation(Lnet/minecraft/world/entity/Entity;FFFFF)V",
-			at = @At("HEAD"), index = 5, ordinal = 3, argsOnly = true)
+			at = @At("HEAD"), ordinal = 3, argsOnly = true)
 	public float pVelocity(float pVelocity, Entity pProjectile) {
-		ServerVivePlayer serverviveplayer = CommonNetworkHelper.vivePlayers.get(pProjectile.getUUID());
-		if (serverviveplayer != null && serverviveplayer.isVR()) {
-			if (((Projectile) (Object) this) instanceof AbstractArrow && !(((Projectile) (Object) this) instanceof ThrownTrident) && !serverviveplayer.isSeated() && serverviveplayer.getDraw() > 0.0F){
+		if (this.serverVivePlayer != null && this.serverVivePlayer.isVR()) {
+			if (((Projectile) (Object) this) instanceof AbstractArrow && !(((Projectile) (Object) this) instanceof ThrownTrident) && !this.serverVivePlayer.isSeated() && this.serverVivePlayer.getDraw() > 0.0F){
 				((AbstractArrow)(Object)this).setBaseDamage(((AbstractArrow)(Object)this).getBaseDamage() * 2.0D);
-				return pVelocity * serverviveplayer.getDraw();
+				return pVelocity * this.serverVivePlayer.getDraw();
 			}
 		}
 		return pVelocity;
