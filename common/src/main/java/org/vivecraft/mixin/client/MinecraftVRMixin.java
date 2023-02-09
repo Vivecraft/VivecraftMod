8fdfba6c85534a28596935c75d5c80529886164d
fayer3 <fayer3@users.noreply.github.com>
2023-02-09 01:39:08 +0100

    fix frame limit removal for forge

--- common/src/main/java/org/vivecraft/mixin/client/MinecraftVRMixin.java
+++ common/src/main/java/org/vivecraft/mixin/client/MinecraftVRMixin.java
@@ -401,9 +401,15 @@ public abstract class MinecraftVRMixin extends ReentrantBlockableEventLoop<Runna
 		callback.cancel();
 	}
 
-	@ModifyConstant(constant = @Constant(longValue = 16), method = "doLoadLevel")
-	private long noWaitOnLevelLoad(long constant) {
-		// the VR runtime handles the frame limit, no need to manually limit it
+	// the VR runtime handles the frame limit, no need to manually limit it 60fps
+	@Group(name = "noFramelimit.", min = 1, max = 1)
+	@ModifyConstant(constant = @Constant(longValue = 16), method = "doLoadLevel", expect = 0)
+	private long noWaitOnLevelLoadFabric(long constant) {
+		return 0L;
+	}
+	@Group(name = "noFramelimit.", min = 1, max = 1)
+	@ModifyConstant(constant = @Constant(longValue = 16), method = "doLoadLevel(Ljava/lang/String;Ljava/util/function/Function;Ljava/util/function/Function;ZLnet/minecraft/client/Minecraft$ExperimentalDialogType;Z)V", expect = 0)
+	private long noWaitOnLevelLoadForge(long constant) {
 		return 0L;
 	}
 
