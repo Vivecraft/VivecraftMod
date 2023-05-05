d3afaecd0b97464b6891098c24f2aefe409b9f34
fayer3 <fayer3@users.noreply.github.com>
2023-05-05 18:56:23 +0200

    fix debug printing of texture names

--- common/src/main/java/org/vivecraft/client_vr/VRTextureTarget.java
+++ common/src/main/java/org/vivecraft/client_vr/VRTextureTarget.java
@@ -6,8 +6,11 @@ import org.vivecraft.client.Xplat;
 import org.vivecraft.client.extensions.RenderTargetExtension;
 
 public class VRTextureTarget extends RenderTarget {
+
+    private final String name;
     public VRTextureTarget(String name, int width, int height, boolean usedepth, boolean onMac, int texid, boolean depthtex, boolean linearFilter, boolean useStencil) {
         super(usedepth);
+        this.name = name;
         RenderSystem.assertOnGameThreadOrInit();
         ((RenderTargetExtension) this).setTextid(texid);
         ((RenderTargetExtension) this).isLinearFilter(linearFilter);
@@ -18,4 +21,18 @@ public class VRTextureTarget extends RenderTarget {
         }
         this.setClearColor(0, 0, 0, 0);
     }
+
+    @Override
+    public String toString() {
+        StringBuilder stringbuilder = new StringBuilder();
+        stringbuilder.append("\n");
+        if (this.name != null) {
+            stringbuilder.append("Name:   " + this.name).append("\n");
+        }
+        stringbuilder.append("Size:   " + this.viewWidth + " x " + this.viewHeight).append("\n");
+        stringbuilder.append("FB ID:  " + this.frameBufferId).append("\n");
+        stringbuilder.append("Tex ID: " + this.colorTextureId).append("\n");
+        return stringbuilder.toString();
+    }
+
 }
