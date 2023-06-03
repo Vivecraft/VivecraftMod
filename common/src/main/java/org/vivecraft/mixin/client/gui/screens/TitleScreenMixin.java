88343d628cebd1e2b8b15fe30dd17915b963c088
fayer3 <fayer3@users.noreply.github.com>
2023-06-04 01:29:32 +0200

    remove unneeded code for restart/warning

--- common/src/main/java/org/vivecraft/mixin/client/gui/screens/TitleScreenMixin.java
+++ common/src/main/java/org/vivecraft/mixin/client/gui/screens/TitleScreenMixin.java
@@ -1,21 +1,16 @@
 package org.vivecraft.mixin.client.gui.screens;
 
 import com.mojang.blaze3d.vertex.PoseStack;
-import net.minecraft.client.gui.GuiComponent;
 import net.minecraft.client.gui.components.*;
 import net.minecraft.client.gui.screens.*;
 import net.minecraft.network.chat.CommonComponents;
 import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.FormattedText;
-import net.minecraft.util.FormattedCharSequence;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.vivecraft.client.utils.UpdateChecker;
 import org.vivecraft.client_vr.VRState;
 import org.vivecraft.client.gui.screens.UpdateScreen;
-
-import java.util.List;
 
 @Mixin(TitleScreen.class)
 public abstract class TitleScreenMixin extends Screen {
@@ -28,7 +23,6 @@ public abstract class TitleScreenMixin extends Screen {
 //    private final Properties vrConfig = new Properties();
 //    private final Path vrConfigPath = Xplat.getConfigPath("vivecraft-config.properties");
     private boolean showError = false;
-    private AbstractWidget firstButton;
     private Button vrModeButton;
 
     private Button updateButton;
@@ -44,14 +38,6 @@ public abstract class TitleScreenMixin extends Screen {
     }
 
     private void addVRModeButton() {
-
-        // get first button, to position warnings
-        for (Renderable widget : renderables) {
-            if (widget instanceof AbstractWidget) {
-                firstButton = (AbstractWidget) widget;
-                break;
-            }
-        }
 
         vrModeButton = new Button.Builder(Component.translatable("vivecraft.gui.vr", getIcon() , VRState.vrEnabled ? CommonComponents.OPTION_ON : CommonComponents.OPTION_OFF), (button) -> {
             showError = false;
@@ -78,41 +64,12 @@ public abstract class TitleScreenMixin extends Screen {
         return (showError ? "§c\u26A0§r " : "");
     }
 
-    @Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screens/Screen;render(Lcom/mojang/blaze3d/vertex/PoseStack;IIF)V", shift = At.Shift.BEFORE, ordinal = 0), method = "render")
-    public void renderText(PoseStack poseStack, int i, int j, float f, CallbackInfo ci) {
-        int l = vrModeButton.getY() - 23;
-        drawString(poseStack, this.font, "Vivecraft", this.width / 2 + 106, l, 16777215);
-        drawString(poseStack, this.font, Component.translatable("vivecraft.messages.mode"), this.width / 2 + 106, l + 10, 16777215);
-    }
-
     @Inject(at = @At("TAIL"), method = "render")
-    public void renderWarning(PoseStack poseStack, int i, int j, float f, CallbackInfo ci) {
-
+    public void renderToolTip(PoseStack poseStack, int i, int j, float f, CallbackInfo ci) {
         updateButton.visible = UpdateChecker.hasUpdate;
 
         if (vrModeButton.isMouseOver(i, j)) {
             renderTooltip(poseStack, font.split(Component.translatable("vivecraft.options.VR_MODE.tooltip"), Math.max(width / 2 - 43, 170)), i, j);
-        }
-
-        int warningHeight = firstButton.getY() - 10;
-        Component warning = null;
-        if (showError) {
-            warning = Component.translatable("vivecraft.messages.configWriteError");
-        }
-        if (warning != null) {
-            int length = 0;
-            List<FormattedCharSequence> splitString = font.split(FormattedText.of(warning.getString()), 360);
-            for (FormattedCharSequence string : splitString) {
-                length = Math.max(length, this.font.width(string));
-            }
-            // move in front of button tooltips
-            poseStack.pushPose();
-            poseStack.translate(0, 0, 500);
-            GuiComponent.fill(poseStack, this.width / 2 - length / 2 - 4, warningHeight - 4, this.width / 2 + length / 2 + 4, warningHeight + (font.lineHeight + 3) * splitString.size(), -536870912);
-            for (int line = 0; line < splitString.size(); line++) {
-                drawCenteredString(poseStack, this.font, splitString.get(line), this.width / 2, warningHeight + (font.lineHeight + 2) * line, 16777215);
-            }
-            poseStack.popPose();
         }
     }
 }
