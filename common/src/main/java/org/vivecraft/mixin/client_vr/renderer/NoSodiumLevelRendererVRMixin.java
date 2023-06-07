f0799792e13683a0d30b52330e923d6d9c97b227
fayer3 <fayer3@users.noreply.github.com>
2023-06-07 19:31:59 +0200

    fix flickering chunks without sodium, by always updating the frustum, even if the chunk update is not complete

--- common/src/main/java/org/vivecraft/mixin/client_vr/renderer/NoSodiumLevelRendererVRMixin.java
+++ common/src/main/java/org/vivecraft/mixin/client_vr/renderer/NoSodiumLevelRendererVRMixin.java
@@ -1,5 +1,6 @@
 package org.vivecraft.mixin.client_vr.renderer;
 
+import org.spongepowered.asm.mixin.Final;
 import org.vivecraft.client_vr.VRState;
 import org.vivecraft.mixin.client.blaze3d.RenderSystemAccessor;
 import net.minecraft.client.Camera;
@@ -13,15 +14,22 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 @Mixin(LevelRenderer.class)
 public class NoSodiumLevelRendererVRMixin {
 
     @Shadow
     private boolean needsFullRenderChunkUpdate;
 
+    @Shadow
+    @Final
+    private AtomicBoolean needsFrustumUpdate;
+
     @Inject(at = @At(value = "FIELD", target = "Lnet/minecraft/client/renderer/LevelRenderer;needsFullRenderChunkUpdate:Z", ordinal = 1, shift = At.Shift.AFTER), method = "setupRender(Lnet/minecraft/client/Camera;Lnet/minecraft/client/renderer/culling/Frustum;ZZ)V")
     public void alwaysUpdateCull(Camera camera, Frustum frustum, boolean bl, boolean bl2, CallbackInfo info) {
         this.needsFullRenderChunkUpdate = needsFullRenderChunkUpdate || VRState.vrRunning;
+        needsFrustumUpdate.set(true);
     }
 
     @ModifyConstant(method = "renderChunkLayer", constant = @Constant(intValue = 12))
