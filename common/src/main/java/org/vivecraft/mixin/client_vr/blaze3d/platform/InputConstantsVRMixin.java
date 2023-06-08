949565802b4b85664938564a3055f8abcba285f9
fayer3 <fayer3@users.noreply.github.com>
2023-06-08 15:06:26 +0200

    fix gui shift being stuck, when when vr is hotswitched off

--- common/src/main/java/org/vivecraft/mixin/client_vr/blaze3d/platform/InputConstantsVRMixin.java
+++ common/src/main/java/org/vivecraft/mixin/client_vr/blaze3d/platform/InputConstantsVRMixin.java
@@ -6,6 +6,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.vivecraft.client_vr.VRState;
 import org.vivecraft.client_vr.provider.InputSimulator;
 
 @Mixin(InputConstants.class)
@@ -13,6 +14,6 @@ public class InputConstantsVRMixin {
 
     @Inject(at = @At("HEAD"), method = "isKeyDown", cancellable = true)
     private static void keyDown(long l, int i, CallbackInfoReturnable<Boolean> cir){
-        cir.setReturnValue(GLFW.glfwGetKey(l,i) == 1 || InputSimulator.isKeyDown(i)) ;
+        cir.setReturnValue(GLFW.glfwGetKey(l,i) == 1 || (VRState.vrRunning && InputSimulator.isKeyDown(i))) ;
     }
 }
