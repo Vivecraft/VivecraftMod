684a966a22d3b977b8e5d8b657986db0cd551d6d
fayer3 <fayer3@users.noreply.github.com>
2023-06-20 17:35:50 +0200

    fix vr controls centered string alignment

--- common/src/main/java/org/vivecraft/client/gui/settings/GuiVRControls.java
+++ common/src/main/java/org/vivecraft/client/gui/settings/GuiVRControls.java
@@ -39,8 +39,12 @@ public class GuiVRControls extends GuiVROptionsBase
     public void render(PoseStack pMatrixStack, int pMouseX, int pMouseY, float pPartialTicks)
     {
         super.render(pMatrixStack, pMouseX, pMouseY, pPartialTicks);
-        drawCenteredString(pMatrixStack, this.minecraft.font, Component.translatable("vivecraft.messages.controls.1"), this.width / 2, this.height / 2 - 9 / 2 - 9 - 3, 16777215);
-        drawCenteredString(pMatrixStack, this.minecraft.font, Component.translatable("vivecraft.messages.controls.2"), this.width / 2, this.height / 2 - 9 / 2, 16777215);
-        drawCenteredString(pMatrixStack, this.minecraft.font, Component.translatable("vivecraft.messages.controls.3"), this.width / 2, this.height / 2 - 9 / 2 + 9 + 3, 16777215);
+
+        int middle = 240 / 2 - this.minecraft.font.lineHeight / 2;
+        int lineHeight = this.minecraft.font.lineHeight + 3;
+
+        drawCenteredString(pMatrixStack, this.minecraft.font, Component.translatable("vivecraft.messages.controls.1"), this.width / 2, middle - lineHeight, 16777215);
+        drawCenteredString(pMatrixStack, this.minecraft.font, Component.translatable("vivecraft.messages.controls.2"), this.width / 2, middle, 16777215);
+        drawCenteredString(pMatrixStack, this.minecraft.font, Component.translatable("vivecraft.messages.controls.3"), this.width / 2, middle + lineHeight, 16777215);
     }
 }
