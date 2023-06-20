9d78fd6480803767eb37784ba80dc517bbca9542
fayer3 <fayer3@users.noreply.github.com>
2023-06-20 16:02:10 +0200

    fix crash on sign edit

--- common/src/main/java/org/vivecraft/mixin/client_vr/gui/screens/inventory/AbstractSignEditScreenVRMixin.java
+++ common/src/main/java/org/vivecraft/mixin/client_vr/gui/screens/inventory/AbstractSignEditScreenVRMixin.java
@@ -5,6 +5,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.vivecraft.client_vr.VRState;
 import org.vivecraft.client_vr.gameplay.screenhandlers.KeyboardHandler;
 
 @Mixin(AbstractSignEditScreen.class)
@@ -12,11 +13,15 @@ public class AbstractSignEditScreenVRMixin {
 
     @Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/entity/SignBlockEntity;setEditable(Z)V", shift = At.Shift.AFTER), method = "init")
     public void showOverlay(CallbackInfo ci) {
-        KeyboardHandler.setOverlayShowing(true);
+        if(VRState.vrRunning) {
+            KeyboardHandler.setOverlayShowing(true);
+        }
     }
 
     @Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/entity/SignBlockEntity;setEditable(Z)V", shift = At.Shift.AFTER), method = "removed")
     public void dontShowOverlay(CallbackInfo ci) {
-        KeyboardHandler.setOverlayShowing(false);
+        if(VRState.vrRunning) {
+            KeyboardHandler.setOverlayShowing(false);
+    }
     }
 }
