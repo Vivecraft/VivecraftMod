7a0ce1f18bde1a991a09271c25f4eeb581a7a72c
fayer3 <fayer3@users.noreply.github.com>
2023-07-15 16:42:41 +0200

    fix book edit screen crashing nonvr

--- common/src/main/java/org/vivecraft/mixin/client_vr/gui/screens/inventory/BookEditScreenVRMixin.java
+++ common/src/main/java/org/vivecraft/mixin/client_vr/gui/screens/inventory/BookEditScreenVRMixin.java
@@ -5,6 +5,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.vivecraft.client_vr.VRState;
 import org.vivecraft.client_vr.gameplay.screenhandlers.KeyboardHandler;
 
 @Mixin(BookEditScreen.class)
@@ -12,6 +13,8 @@ public class BookEditScreenVRMixin {
 
     @Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screens/inventory/BookEditScreen;updateButtonVisibility()V", shift = At.Shift.BEFORE), method = "init")
     public void overlay(CallbackInfo ci) {
-        KeyboardHandler.setOverlayShowing(true);
+        if (VRState.vrRunning) {
+            KeyboardHandler.setOverlayShowing(true);
+        }
     }
 }
