79cb39730e0d41faacb96c4b11ecbbd7fa4df559
hammy275 <hammy275@gmail.com>
2023-08-23 18:08:08 -0400

    Haptic Pulse and Easy Seated/Reversed Hands Checking for Client API

--- common/src/main/java/org/vivecraft/client/api_impl/ClientAPIImpl.java
+++ common/src/main/java/org/vivecraft/client/api_impl/ClientAPIImpl.java
@@ -4,6 +4,7 @@ import org.vivecraft.api_beta.client.VivecraftClientAPI;
 import org.vivecraft.api_beta.data.VRData;
 import org.vivecraft.client_vr.ClientDataHolderVR;
 import org.vivecraft.client_vr.VRState;
+import org.vivecraft.client_vr.provider.ControllerType;
 import org.vivecraft.client_xr.render_pass.RenderPassType;
 
 public final class ClientAPIImpl implements VivecraftClientAPI {
@@ -51,6 +52,35 @@ public final class ClientAPIImpl implements VivecraftClientAPI {
             throw new IllegalStateException();
         }
         return ClientDataHolderVR.getInstance().vrPlayer.vrdata_world_render.asVRData();
+    }
+
+    @Override
+    public void triggerHapticPulse(int controllerNum, float duration, float frequency, float amplitude, float delay) {
+        if (controllerNum != 0 && controllerNum != 1) {
+            throw new IllegalArgumentException("Can only trigger a haptic pulse for controllers 0 and 1.");
+        }
+        if (amplitude < 0F || amplitude > 1F) {
+            throw new IllegalArgumentException("The amplitude of a haptic pulse must be between 0 and 1.");
+        }
+        if (isVrActive() && !isSeated()) {
+            ClientDataHolderVR.getInstance().vr.triggerHapticPulse(
+                    ControllerType.values()[controllerNum],
+                    duration,
+                    frequency,
+                    amplitude,
+                    delay
+            );
+        }
+    }
+
+    @Override
+    public boolean isSeated() {
+        return ClientDataHolderVR.getInstance().vrSettings.seated;
+    }
+
+    @Override
+    public boolean usingReversedHands() {
+        return ClientDataHolderVR.getInstance().vrSettings.reverseHands;
     }
 
     @Override
