bb4ac3558fbceda545c464e04287f9230b1149bc
fayer3 <fayer3@users.noreply.github.com>
2023-09-30 18:07:04 +0200

    move welcome message to PlayerList to be consistent across versions

--- common/src/main/java/org/vivecraft/mixin/server/players/PlayerListMixin.java
+++ common/src/main/java/org/vivecraft/mixin/server/players/PlayerListMixin.java
@@ -1,18 +1,19 @@
-package org.vivecraft.mixin.server;
+package org.vivecraft.mixin.server.players;
 
+import net.minecraft.network.Connection;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.server.network.ServerLoginPacketListenerImpl;
+import net.minecraft.server.players.PlayerList;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.vivecraft.server.ServerUtil;
 
-@Mixin(ServerLoginPacketListenerImpl.class)
-public class ServerLoginPacketListenerImplMixin {
+@Mixin(PlayerList.class)
+public class PlayerListMixin {
 
     @Inject(at = @At("HEAD"), method = "placeNewPlayer")
-    private void vivecraft$scheduleLoginMessages(ServerPlayer serverPlayer, CallbackInfo ci) {
+    private void vivecraft$scheduleLoginMessages(Connection connection, ServerPlayer serverPlayer, CallbackInfo ci) {
         ServerUtil.scheduleWelcomeMessageOrKick(serverPlayer);
         ServerUtil.sendUpdateNotificationIfOP(serverPlayer);
     }
