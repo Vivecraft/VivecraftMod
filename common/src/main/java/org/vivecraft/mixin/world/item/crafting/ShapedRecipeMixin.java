62ad7f08c21b2a67720d12787d09a9d0eba0be79
ferriarnus <61201275+ferriarnus@users.noreply.github.com>
2023-10-01 00:31:17 +0200

    New codec based on either codec

--- common/src/main/java/org/vivecraft/mixin/world/item/crafting/ShapedRecipeMixin.java
+++ common/src/main/java/org/vivecraft/mixin/world/item/crafting/ShapedRecipeMixin.java
@@ -1,16 +1,29 @@
 package org.vivecraft.mixin.world.item.crafting;
 
+import com.mojang.datafixers.kinds.App;
+import com.mojang.datafixers.util.Either;
 import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.crafting.ShapedRecipe;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.vivecraft.common.CustomShapedRecipe;
 
+import java.util.function.Function;
+
 @Mixin(targets = "net.minecraft.world.item.crafting.ShapedRecipe$Serializer$RawShapedRecipe")
-public class ShapedRecipeMixin {
+public abstract class ShapedRecipeMixin {
+
     @Redirect(at = @At(value = "FIELD", target = "Lnet/minecraft/world/item/crafting/CraftingRecipeCodecs;ITEMSTACK_OBJECT_CODEC:Lcom/mojang/serialization/Codec;"), method = "method_53750")
-    private static Codec vivecraft$handleVivecraftRecipe() {
+    private static Codec<ItemStack> vivecraft$handleVivecraftRecipe() {
         // CODEC needs to be external, or it isn't initialized, when the other static codec want's to access it
-        return CustomShapedRecipe.VIVECRAFT_ITEMSTACK_OBJECT_CODEC;
+        return CustomShapedRecipe.CODEC;
     }
 }
