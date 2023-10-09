af284abc2ed01f2ee1dd5acafbe193518121f8aa
fayer3 <fayer3@users.noreply.github.com>
2023-10-09 22:38:45 +0200

    move forge mod config screen registry to client only

--- forge/src/main/java/org/vivecraft/forge/event/ClientEvents.java
+++ forge/src/main/java/org/vivecraft/forge/event/ClientEvents.java
@@ -1,14 +1,19 @@
 package org.vivecraft.forge.event;
 
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.ConfigScreenHandler;
 import net.minecraftforge.client.event.ModelEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLConstructModEvent;
+import org.vivecraft.client.gui.settings.VivecraftMainSettings;
 import org.vivecraft.client_vr.ClientDataHolderVR;
 import org.vivecraft.client_vr.gameplay.trackers.CameraTracker;
 import org.vivecraft.client_vr.gameplay.trackers.TelescopeTracker;
+import org.vivecraft.forge.Vivecraft;
 
-@Mod.EventBusSubscriber(value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
+@Mod.EventBusSubscriber(value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD, modid = Vivecraft.MODID)
 public class ClientEvents {
     @SubscribeEvent
     public static void registerModels(ModelEvent.RegisterAdditional event) {
@@ -17,5 +22,10 @@ public class ClientEvents {
         event.register(ClientDataHolderVR.thirdPersonCameraDisplayModel);
         event.register(CameraTracker.cameraModel);
         event.register(CameraTracker.cameraDisplayModel);
+    }
+
+    @SubscribeEvent
+    public static void registerConfigScreen(FMLConstructModEvent constructModEvent) {
+        ModLoadingContext.get().registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory.class, () -> new ConfigScreenHandler.ConfigScreenFactory((mc, screen) -> new VivecraftMainSettings(screen)));
     }
 }
