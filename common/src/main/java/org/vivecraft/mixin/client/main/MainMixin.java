a2b8b15c78c800ba3d4a6d0aed917d09306a3caf
fayer3 <fayer3@users.noreply.github.com>
2023-10-11 04:21:15 +0200

    fix crash with forge 48.0.14+, bump to 1.1.1

--- common/src/main/java/org/vivecraft/mixin/client/main/MainMixin.java
+++ common/src/main/java/org/vivecraft/mixin/client/main/MainMixin.java
@@ -1,31 +1,26 @@
 package org.vivecraft.mixin.client.main;
 
-import com.google.common.base.Stopwatch;
 import joptsimple.OptionParser;
 import joptsimple.OptionSet;
 import net.minecraft.client.main.Main;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 import org.vivecraft.client_vr.ClientDataHolderVR;
 
 @Mixin(Main.class)
 public class MainMixin {
 
-    @Inject(at = @At(value = "INVOKE", target = "Ljoptsimple/OptionParser;allowsUnrecognizedOptions()V"), method = "main", locals = LocalCapture.CAPTURE_FAILHARD, remap = false)
-    private static void vivecraft$options(String[] strings, CallbackInfo ci, Stopwatch stopwatch, Stopwatch stopwatch2, OptionParser optionparser) {
-        optionparser.accepts("kiosk");
-        optionparser.accepts("viewonly");
-        optionparser.accepts("katvr");
-        optionparser.accepts("infinadeck");
-    }
-
-    @Redirect(at = @At(value = "INVOKE", target = "Ljoptsimple/OptionParser;parse([Ljava/lang/String;)Ljoptsimple/OptionSet;", remap = false), method = "main", remap = false)
-    private static OptionSet vivecraft$kiosk(OptionParser optionparser, String[] p_129642_) {
-        OptionSet optionset = optionparser.parse(p_129642_);
+    @Inject(at = @At("HEAD"), method = "main", remap = false)
+    private static void vivecraft$options(String[] strings, CallbackInfo ci) {
+        OptionParser vivecraftOptionParser = new OptionParser();
+        vivecraftOptionParser.allowsUnrecognizedOptions();
+        vivecraftOptionParser.accepts("kiosk");
+        vivecraftOptionParser.accepts("viewonly");
+        vivecraftOptionParser.accepts("katvr");
+        vivecraftOptionParser.accepts("infinadeck");
+        OptionSet optionset = vivecraftOptionParser.parse(strings);
         ClientDataHolderVR.kiosk = optionset.has("kiosk");
 
         if (ClientDataHolderVR.kiosk) {
@@ -42,6 +37,5 @@ public class MainMixin {
 
         ClientDataHolderVR.katvr = optionset.has("katvr");
         ClientDataHolderVR.infinadeck = optionset.has("infinadeck");
-        return optionset;
     }
 }
