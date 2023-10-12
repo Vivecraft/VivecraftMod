c596f720bdd1be644759b5ab18fc53910827a5bf
fayer3 <fayer3@users.noreply.github.com>
2023-10-12 02:03:40 +0200

    fix slow memory leak, fixes some stuff pre 1.19.4

--- common/src/main/java/org/vivecraft/mixin/client_vr/MinecraftVRMixin.java
+++ common/src/main/java/org/vivecraft/mixin/client_vr/MinecraftVRMixin.java
@@ -368,6 +368,7 @@ public abstract class MinecraftVRMixin implements MinecraftExtension {
             this.mainRenderTarget.bindWrite(true);
 
             // draw screen/gui to buffer
+            // push pose so we can pop it later
             RenderSystem.getModelViewStack().pushPose();
             ((GameRendererExtension) this.gameRenderer).vivecraft$setShouldDrawScreen(true);
             // only draw the gui when the level was rendered once, since some mods expect that
@@ -402,6 +403,10 @@ public abstract class MinecraftVRMixin implements MinecraftExtension {
             vivecraft$drawProfiler();
             // reset that, do not draw it again on something else
             fpsPieResults = null;
+
+            // pop pose that we pushed before the gui
+            RenderSystem.getModelViewStack().popPose();
+            RenderSystem.applyModelViewMatrix();
 
             // generate mipmaps
             // TODO: does this do anything?
