45ded58bf76dab569fa3400ed1d372e5fa8dbbcf
fayer3 <fayer3@users.noreply.github.com>
2023-10-29 23:45:49 +0100

    Forge I hate you Fix kick on death on spigot/paper servers

--- forge/src/main/java/org/vivecraft/client/forge/XplatImpl.java
+++ forge/src/main/java/org/vivecraft/client/forge/XplatImpl.java
@@ -1,9 +1,13 @@
 package org.vivecraft.client.forge;
 
 import com.mojang.blaze3d.pipeline.RenderTarget;
+import io.netty.buffer.Unpooled;
 import net.minecraft.client.multiplayer.ClientPacketListener;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.network.protocol.common.ServerboundCustomPayloadPacket;
+import net.minecraft.network.protocol.common.custom.DiscardedPayload;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.BlockAndTintGetter;
 import net.minecraft.world.level.biome.Biome;
@@ -15,7 +19,6 @@ import net.minecraftforge.fml.loading.FMLEnvironment;
 import net.minecraftforge.fml.loading.FMLLoader;
 import net.minecraftforge.fml.loading.FMLPaths;
 import net.minecraftforge.fml.util.ObfuscationReflectionHelper;
-import net.minecraftforge.network.ChannelListManager;
 
 import java.nio.file.Path;
 
@@ -82,6 +85,10 @@ public class XplatImpl {
     }
 
     public static void addNetworkChannel(ClientPacketListener listener, ResourceLocation resourceLocation) {
-        ChannelListManager.addChannels(listener.getConnection(), resourceLocation);
+        // Forge I really don't know why you are insisting on this being a DiscardedPayload
+        listener.send(new ServerboundCustomPayloadPacket(new DiscardedPayload(
+            new ResourceLocation("minecraft:register"),
+            new FriendlyByteBuf(Unpooled.buffer())
+                .writeBytes(resourceLocation.toString().getBytes()))));
     }
 }
