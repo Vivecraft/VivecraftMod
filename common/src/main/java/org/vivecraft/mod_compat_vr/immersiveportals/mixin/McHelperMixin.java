17930b8bb9b8c51bdf1e3c05268f603b74bea7b1
hammy275 <hammy275@gmail.com>
2024-02-10 15:26:10 -0500

    Immersive Portals Working

---
+++ common/src/main/java/org/vivecraft/mod_compat_vr/immersiveportals/mixin/McHelperMixin.java
@@ -0,0 +1,21 @@
+package org.vivecraft.mod_compat_vr.immersiveportals.mixin;
+
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.phys.Vec3;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.vivecraft.client_vr.gameplay.VRPlayer;
+import qouteall.imm_ptl.core.McHelper;
+
+@Mixin(McHelper.class)
+public class McHelperMixin {
+
+    @Inject(at = @At("HEAD"), method = "updateBoundingBox")
+    private static void updateBoundingBox(Entity player, CallbackInfo ci) {
+        Vec3 newPos = player.position();
+        VRPlayer.get().setRoomOrigin(newPos.x, newPos.y, newPos.z, true);
+    }
+
+}
