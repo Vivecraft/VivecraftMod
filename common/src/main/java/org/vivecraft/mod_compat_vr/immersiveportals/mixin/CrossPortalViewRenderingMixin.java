431e1176beebf24f532be16b396f8fc7cf932ae3
hammy275 <hammy275@gmail.com>
2024-02-11 11:53:18 -0500

    A More 'Correct' CrossPortalViewRendering Mixin

--- common/src/main/java/org/vivecraft/mod_compat_vr/immersiveportals/mixin/CrossPortalViewRenderingMixin.java
+++ common/src/main/java/org/vivecraft/mod_compat_vr/immersiveportals/mixin/CrossPortalViewRenderingMixin.java
@@ -14,16 +14,16 @@ import qouteall.imm_ptl.core.teleportation.ClientTeleportationManager;
 @Mixin(CrossPortalViewRendering.class)
 public class CrossPortalViewRenderingMixin {
 
-    @Redirect(method = "renderCrossPortalView", at = @At(value = "INVOKE", target = "Lqouteall/imm_ptl/core/teleportation/ClientTeleportationManager;getPlayerEyePos(F)Lnet/minecraft/world/phys/Vec3;"), remap = false)
-    private static Vec3 getPlayerEyePos(float partialTick) {
+    @Redirect(method = "renderCrossPortalView", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/Camera;getPosition()Lnet/minecraft/world/phys/Vec3;"), remap = false)
+    private static Vec3 getPlayerCameraPos(Camera camera) {
         if (VRState.vrRunning && ClientDataHolderVR.getInstance() != null && VRPlayer.get() != null) {
             return VRPlayer.get().vrdata_world_render.getEye(ClientDataHolderVR.getInstance().currentPass).getPosition();
         }
-        return ClientTeleportationManager.getPlayerEyePos(partialTick);
+        return camera.getPosition();
     }
 
-    @Redirect(method = "renderCrossPortalView", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/Camera;getPosition()Lnet/minecraft/world/phys/Vec3;"), remap = false)
-    private static Vec3 getPlayerCameraPos(Camera camera) {
+    @Redirect(method = "renderCrossPortalView", at = @At(value = "INVOKE", target = "Lqouteall/imm_ptl/core/render/TransformationManager;getIsometricAdjustedCameraPos(Lnet/minecraft/client/Camera;)Lnet/minecraft/world/phys/Vec3;"), remap = false)
+    private static Vec3 getPlayerCameraPos2(Camera camera) {
         if (VRState.vrRunning && ClientDataHolderVR.getInstance() != null && VRPlayer.get() != null) {
             return VRPlayer.get().vrdata_world_render.getEye(ClientDataHolderVR.getInstance().currentPass).getPosition();
         }
