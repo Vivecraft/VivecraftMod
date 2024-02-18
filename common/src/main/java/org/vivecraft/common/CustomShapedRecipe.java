d7962211a16f466c8cf64546bfb0d7803bd362db
fayer3 <bayer.florian@aon.at>
2024-02-18 23:50:51 +0100

    1.20.2 fixes

--- common/src/main/java/org/vivecraft/common/CustomShapedRecipe.java
+++ common/src/main/java/org/vivecraft/common/CustomShapedRecipe.java
@@ -23,18 +23,33 @@ public class CustomShapedRecipe {
                 .strictOptionalField(ExtraCodecs.COMPONENT, "name", Component.empty())
                 .forGetter(ItemStack::getHoverName),
             ExtraCodecs
+                .strictOptionalField(ExtraCodecs.COMPONENT, "fallbackname", Component.empty())
+                .forGetter(ItemStack::getHoverName),
+            ExtraCodecs
                 .strictOptionalField(Codec.BOOL, "unbreakable", false)
                 .forGetter(itemStack -> itemStack.getOrCreateTag().getBoolean("Unbreakable")),
             ExtraCodecs
                 .strictOptionalField(ExtraCodecs.POSITIVE_INT, "hideflags", 0)
-                .forGetter(itemStack -> itemStack.getOrCreateTag().getInt("HideFlags"))
-        ).apply(instance, (vanillaitem, count, name, unbreakable, hideflags) -> {
+                .forGetter(itemStack -> itemStack.getOrCreateTag().getInt("HideFlags")),
+            ExtraCodecs
+                .strictOptionalField(ExtraCodecs.POSITIVE_INT, "color", -1)
+                .forGetter(itemStack -> itemStack.getOrCreateTagElement(ItemStack.TAG_DISPLAY).getInt(ItemStack.TAG_COLOR))
+        ).apply(instance, (vanillaitem, count, name, fallbackname, unbreakable, hideflags, color) -> {
             ItemStack itemStack = new ItemStack(vanillaitem, count);
             if (!name.getString().isEmpty()) {
-                itemStack.setHoverName(Component.translatable(name.getString()));
+                if (!fallbackname.getString().isEmpty()) {
+                    itemStack.setHoverName(Component.translatableWithFallback(
+                        name.getString(),
+                        fallbackname.getString()));
+                } else {
+                    itemStack.setHoverName(Component.translatable(name.getString()));
+                }
             }
             itemStack.getOrCreateTag().putInt("HideFlags", hideflags);
             itemStack.getOrCreateTag().putBoolean("Unbreakable", unbreakable);
+            if (color > -1) {
+                itemStack.getOrCreateTagElement(ItemStack.TAG_DISPLAY).putInt(ItemStack.TAG_COLOR, color);
+            }
             return itemStack;
         })
     );
