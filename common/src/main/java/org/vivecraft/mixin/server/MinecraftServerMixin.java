73ee002018c72038b34003b7c16899c527cd923e
fayer3 <bayer.florian@aon.at>
2024-06-17 00:59:05 +0200

    manually shut down the message scheduler on server shutdown, and don't rely on the runtime hook. fixes #283

--- common/src/main/java/org/vivecraft/mixin/server/MinecraftServerMixin.java
+++ common/src/main/java/org/vivecraft/mixin/server/MinecraftServerMixin.java
@@ -3,7 +3,13 @@ package org.vivecraft.mixin.server;
 import net.minecraft.server.MinecraftServer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.vivecraft.client.Xplat;
 import org.vivecraft.server.MinecraftServerExt;
+import org.vivecraft.server.ServerNetworking;
+import org.vivecraft.server.ServerUtil;
 import org.vivecraft.server.ServerVivePlayer;
 
 import java.util.HashMap;
@@ -20,5 +26,13 @@ public class MinecraftServerMixin implements MinecraftServerExt {
     @Unique
     public Map<UUID, ServerVivePlayer> vivecraft$getPlayersWithVivecraft() {
         return this.vivecraft$playersWithVivecraft;
+    }
+
+    @Inject(at = @At("HEAD"), method = "stopServer")
+    private void vivecraft$stopExecutor(CallbackInfo ci) {
+        if (Xplat.isDedicatedServer()) {
+            ServerNetworking.LOGGER.info("shutting down vivecraft scheduler");
+            ServerUtil.scheduler.shutdownNow();
+        }
     }
 }
