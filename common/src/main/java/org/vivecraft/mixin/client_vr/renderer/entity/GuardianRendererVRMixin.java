4f0e9e41be3b1f762d1db21ac2ebeb72e6e1909e
fayer3 <bayer.florian@aon.at>
2024-09-04 01:31:01 +0200

    cleanup and switch to wrap operation

--- common/src/main/java/org/vivecraft/mixin/client_vr/renderer/entity/GuardianRendererVRMixin.java
+++ common/src/main/java/org/vivecraft/mixin/client_vr/renderer/entity/GuardianRendererVRMixin.java
@@ -1,13 +1,13 @@
 package org.vivecraft.mixin.client_vr.renderer.entity;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.entity.GuardianRenderer;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.vivecraft.client_vr.ClientDataHolderVR;
 import org.vivecraft.client_vr.render.RenderPass;
 import org.vivecraft.client_xr.render_pass.RenderPassType;
@@ -15,14 +15,15 @@ import org.vivecraft.client_xr.render_pass.RenderPassType;
 @Mixin(GuardianRenderer.class)
 public abstract class GuardianRendererVRMixin {
 
-    @Shadow
-    protected abstract Vec3 getPosition(LivingEntity livingEntity, double d, float f);
-
-    @Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/entity/GuardianRenderer;getPosition(Lnet/minecraft/world/entity/LivingEntity;DF)Lnet/minecraft/world/phys/Vec3;"), method = "render(Lnet/minecraft/world/entity/monster/Guardian;FFLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;I)V")
-    public Vec3 vivecraft$changeEye(GuardianRenderer instance, LivingEntity livingEntity, double d, float f) {
+    @WrapOperation(method = "render(Lnet/minecraft/world/entity/monster/Guardian;FFLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;I)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/entity/GuardianRenderer;getPosition(Lnet/minecraft/world/entity/LivingEntity;DF)Lnet/minecraft/world/phys/Vec3;", ordinal = 0))
+    private Vec3 vivecraft$changeTargetPos(
+        GuardianRenderer instance, LivingEntity livingEntity, double yOffset, float partialTick,
+        Operation<Vec3> original)
+    {
         if (!RenderPassType.isVanilla() && livingEntity == Minecraft.getInstance().getCameraEntity()) {
-            return ClientDataHolderVR.getInstance().vrPlayer.getVRDataWorld().getEye(RenderPass.CENTER).getPosition().subtract(0.0D, 0.3D * (double) ClientDataHolderVR.getInstance().vrPlayer.worldScale, 0.0D);
+            return ClientDataHolderVR.getInstance().vrPlayer.getVRDataWorld().getEye(RenderPass.CENTER).getPosition().subtract(0.0D, 0.3F * ClientDataHolderVR.getInstance().vrPlayer.worldScale, 0.0D);
+        } else {
+            return original.call(instance, livingEntity, yOffset, partialTick);
         }
-        return this.getPosition(livingEntity, d, f);
     }
 }
