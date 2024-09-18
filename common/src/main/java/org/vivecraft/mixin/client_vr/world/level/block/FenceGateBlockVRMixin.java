8226c1cbed9b5a69261b299593a88a2d19ff45d4
fayer3 <bayer.florian@aon.at>
2024-09-18 21:22:35 +0200

    cleanup doors and boat limit roomscale boat speed to vanilla value, and add gradual turning when pointing

--- common/src/main/java/org/vivecraft/mixin/client_vr/world/level/block/FenceGateBlockVRMixin.java
+++ common/src/main/java/org/vivecraft/mixin/client_vr/world/level/block/FenceGateBlockVRMixin.java
@@ -1,37 +1,42 @@
 package org.vivecraft.mixin.client_vr.world.level.block;
 
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.client.Minecraft;
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.FenceGateBlock;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.phys.BlockHitResult;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 import org.vivecraft.client_vr.ClientDataHolderVR;
 import org.vivecraft.client_vr.VRState;
 
 @Mixin(FenceGateBlock.class)
 public class FenceGateBlockVRMixin {
 
-    @Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;playSound(Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/core/BlockPos;Lnet/minecraft/sounds/SoundEvent;Lnet/minecraft/sounds/SoundSource;FF)V"), method = "use", locals = LocalCapture.CAPTURE_FAILHARD)
-    public void vivecraft$hapticFeedbackOnClose1(BlockState blockState, Level level, BlockPos blockPos, Player player, InteractionHand interactionHand, BlockHitResult blockHitResult, CallbackInfoReturnable<InteractionResult> cir, boolean opening) {
-        if (VRState.vrRunning && !opening && Minecraft.getInstance().player != null && Minecraft.getInstance().player.isAlive() && Minecraft.getInstance().player.blockPosition().distSqr(blockPos) < 25.0D) {
+    @Inject(method = "use", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;playSound(Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/core/BlockPos;Lnet/minecraft/sounds/SoundEvent;Lnet/minecraft/sounds/SoundSource;FF)V"))
+    private void vivecraft$hapticFeedbackOnClose1(
+        CallbackInfoReturnable<InteractionResult> cir, @Local(argsOnly = true) BlockPos pos,
+        @Local boolean opening)
+    {
+        if (VRState.vrRunning && !opening && Minecraft.getInstance().player != null &&
+            Minecraft.getInstance().player.isAlive() &&
+            Minecraft.getInstance().player.blockPosition().distSqr(pos) < 25.0D)
+        {
             ClientDataHolderVR.getInstance().vr.triggerHapticPulse(0, 250);
         }
     }
 
-    @Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;playSound(Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/core/BlockPos;Lnet/minecraft/sounds/SoundEvent;Lnet/minecraft/sounds/SoundSource;FF)V"), method = "neighborChanged", locals = LocalCapture.CAPTURE_FAILHARD)
-    public void vivecraft$hapticFeedbackOnClose2(BlockState blockState, Level level, BlockPos blockPos, Block block, BlockPos blockPos2, boolean bl, CallbackInfo ci, boolean opening) {
-        if (VRState.vrRunning && !opening && Minecraft.getInstance().player != null && Minecraft.getInstance().player.isAlive() && Minecraft.getInstance().player.blockPosition().distSqr(blockPos) < 25.0D) {
+    @Inject(method = "neighborChanged", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;playSound(Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/core/BlockPos;Lnet/minecraft/sounds/SoundEvent;Lnet/minecraft/sounds/SoundSource;FF)V"))
+    private void vivecraft$hapticFeedbackOnClose2(
+        CallbackInfo ci, @Local(argsOnly = true, ordinal = 0) BlockPos pos, @Local(ordinal = 1) boolean opening)
+    {
+        if (VRState.vrRunning && !opening && Minecraft.getInstance().player != null &&
+            Minecraft.getInstance().player.isAlive() &&
+            Minecraft.getInstance().player.blockPosition().distSqr(pos) < 25.0D)
+        {
             ClientDataHolderVR.getInstance().vr.triggerHapticPulse(0, 250);
         }
     }
