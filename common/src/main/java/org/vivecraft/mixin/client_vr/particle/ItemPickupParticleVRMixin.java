29b1061fae1183344e1f73880f5e7a84cdebca2e
fayer3 <bayer.florian@aon.at>
2024-09-26 17:24:10 +0200

    fix ItemPickupParticle offset

--- common/src/main/java/org/vivecraft/mixin/client_vr/particle/ItemPickupParticleVRMixin.java
+++ common/src/main/java/org/vivecraft/mixin/client_vr/particle/ItemPickupParticleVRMixin.java
@@ -12,10 +12,8 @@ import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.vivecraft.client.Xplat;
 import org.vivecraft.client_vr.VRState;
 import org.vivecraft.client_vr.render.helpers.RenderHelper;
-import org.vivecraft.mod_compat_vr.pehkui.PehkuiHelper;
 
 @Mixin(ItemPickupParticle.class)
 public class ItemPickupParticleVRMixin {
@@ -32,7 +30,7 @@ public class ItemPickupParticleVRMixin {
         double delta, double start, double end, Operation<Double> original,
         @Share("controllerPos") LocalRef<Vec3> controllerPos)
     {
-        if (VRState.vrRunning && target == Minecraft.getInstance().player) {
+        if (VRState.vrRunning && this.target == Minecraft.getInstance().player) {
             controllerPos.set(RenderHelper.getControllerRenderPos(0));
             return controllerPos.get().x;
         } else {
@@ -45,18 +43,9 @@ public class ItemPickupParticleVRMixin {
         double delta, double start, double end, Operation<Double> original,
         @Share("controllerPos") LocalRef<Vec3> controllerPos)
     {
-        if (VRState.vrRunning && target == Minecraft.getInstance().player) {
-            float offset = 0.5F;
-            if (Xplat.isModLoaded("pehkui")) {
-                // pehkui changes the offset, need to account for that
-                offset *= PehkuiHelper.getPlayerScale(target, (float) delta);
-            }
-            // offset, so the particle is centered around the arm
-            offset += itemEntity.getBbHeight();
-            return controllerPos.get().y - offset;
-        } else {
-            return original.call(delta, start, end);
-        }
+        return controllerPos.get() != null ?
+            controllerPos.get().y - this.itemEntity.getBbHeight() :
+            original.call(delta, start, end);
     }
 
     @WrapOperation(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/Mth;lerp(DDD)D", ordinal = 2))
@@ -64,10 +53,6 @@ public class ItemPickupParticleVRMixin {
         double delta, double start, double end, Operation<Double> original,
         @Share("controllerPos") LocalRef<Vec3> controllerPos)
     {
-        if (VRState.vrRunning && target == Minecraft.getInstance().player) {
-            return  controllerPos.get().z;
-        } else {
-            return original.call(delta, start, end);
-        }
+        return controllerPos.get() != null ? controllerPos.get().z : original.call(delta, start, end);
     }
 }
