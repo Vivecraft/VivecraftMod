304c1a6eb342d90af90618563260922749c07e5c
fayer3 <bayer.florian@aon.at>
2024-09-30 16:13:18 +0200

    cleanup projectiles fix arrow speed ignoring roomscale bow draw

--- common/src/main/java/org/vivecraft/mixin/world/entity/projectile/AbstractArrowMixin.java
+++ common/src/main/java/org/vivecraft/mixin/world/entity/projectile/AbstractArrowMixin.java
@@ -1,5 +1,6 @@
 package org.vivecraft.mixin.world.entity.projectile;
 
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.network.protocol.game.ClientboundSoundPacket;
@@ -12,7 +13,6 @@ import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.projectile.AbstractArrow;
 import net.minecraft.world.entity.projectile.Projectile;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.EntityHitResult;
@@ -31,39 +31,39 @@ import org.vivecraft.server.config.ServerConfig;
 @Mixin(AbstractArrow.class)
 public abstract class AbstractArrowMixin extends Entity {
 
-    protected AbstractArrowMixin(EntityType<? extends Projectile> p_37248_, Level p_37249_) {
-        super(p_37248_, p_37249_);
-        // TODO Auto-generated constructor stub
-    }
-
     @Shadow
     private double baseDamage;
 
-    @Inject(at = @At("RETURN"), method = "<init>(Lnet/minecraft/world/entity/EntityType;Lnet/minecraft/world/entity/LivingEntity;Lnet/minecraft/world/level/Level;Lnet/minecraft/world/item/ItemStack;)V")
-    public void vivecraft$pickup(EntityType<? extends AbstractArrow> p_36717_, LivingEntity p_36718_, Level p_36719_, ItemStack itemStack, CallbackInfo info) {
-        if (p_36718_ instanceof ServerPlayer player) {
-            ServerVivePlayer serverviveplayer = ServerVRPlayers.getVivePlayer(player);
-            if (serverviveplayer != null && serverviveplayer.isVR()) {
-                Vec3 vec3 = serverviveplayer.getControllerPos(serverviveplayer.activeHand);
-                Vec3 vec31 = serverviveplayer.getControllerDir(serverviveplayer.activeHand);
+    public AbstractArrowMixin(EntityType<?> entityType, Level level) {
+        super(entityType, level);
+    }
 
-                if (!serverviveplayer.isSeated() && serverviveplayer.draw > 0.0F) {
-                    vec31 = serverviveplayer.getControllerPos(1).subtract(serverviveplayer.getControllerPos(0)).normalize();
-                    vec3 = serverviveplayer.getControllerPos(0);
+    @Inject(method = "<init>(Lnet/minecraft/world/entity/EntityType;Lnet/minecraft/world/entity/LivingEntity;Lnet/minecraft/world/level/Level;Lnet/minecraft/world/item/ItemStack;)V", at = @At("RETURN"))
+    private void vivecraft$startPos(CallbackInfo ci, @Local(argsOnly = true) LivingEntity owner) {
+        if (owner instanceof ServerPlayer player) {
+            ServerVivePlayer serverVivePlayer = ServerVRPlayers.getVivePlayer(player);
+            if (serverVivePlayer != null && serverVivePlayer.isVR()) {
+                Vec3 aimPos = serverVivePlayer.getControllerPos(serverVivePlayer.activeHand);
+                Vec3 aimDir = serverVivePlayer.getControllerDir(serverVivePlayer.activeHand);
+
+                if (!serverVivePlayer.isSeated() && serverVivePlayer.draw > 0.0F) {
+                    aimDir = serverVivePlayer.getControllerPos(1)
+                        .subtract(serverVivePlayer.getControllerPos(0)).normalize();
+                    aimPos = serverVivePlayer.getControllerPos(0);
                 }
 
-                this.setPos(vec3.x + vec31.x, vec3.y + vec31.y, vec3.z + vec31.z);
+                this.setPos(aimPos.x + aimDir.x, aimPos.y + aimDir.y, aimPos.z + aimDir.z);
             }
         }
     }
 
-    @Inject(at = @At("HEAD"), method = "onHitEntity")
-    public void vivecraft$damageMultiplier(EntityHitResult entityHitResult, CallbackInfo ci) {
+    @Inject(method = "onHitEntity", at = @At("HEAD"))
+    private void vivecraft$damageMultiplier(EntityHitResult entityHitResult, CallbackInfo ci) {
         if (((Projectile) (Object) this).getOwner() instanceof ServerPlayer owner) {
             ServerVivePlayer serverVivePlayer = ServerVRPlayers.getVivePlayer(owner);
-            Vec3 hitpos;
+            Vec3 hitPos;
             double multiplier = 1.0;
-            if ((hitpos = vivecraft$isHeadshot(entityHitResult)) != null) {
+            if ((hitPos = vivecraft$isHeadshot(entityHitResult)) != null) {
                 if (serverVivePlayer != null && serverVivePlayer.isVR()) {
                     if (serverVivePlayer.isSeated()) {
                         multiplier = ServerConfig.bowSeatedHeadshotMultiplier.get();
@@ -80,16 +80,15 @@ public abstract class AbstractArrowMixin extends Entity {
                         owner,
                         ParticleTypes.CRIT,
                         true, // always render the hit particles on the client
-                        hitpos.x,
-                        hitpos.y,
-                        hitpos.z,
+                        hitPos.x, hitPos.y, hitPos.z,
                         5,
-                        -this.getDeltaMovement().x,
-                        -this.getDeltaMovement().y,
-                        -this.getDeltaMovement().z,
+                        -this.getDeltaMovement().x, -this.getDeltaMovement().y, -this.getDeltaMovement().z,
                         0.1);
                     // send sound effect
-                    owner.connection.send(new ClientboundSoundPacket(BuiltInRegistries.SOUND_EVENT.wrapAsHolder(SoundEvents.ITEM_BREAK), SoundSource.PLAYERS, owner.getX(), owner.getY(), owner.getZ(), 0.7f, 0.5f, owner.level().random.nextLong()));
+                    owner.connection.send(
+                        new ClientboundSoundPacket(BuiltInRegistries.SOUND_EVENT.wrapAsHolder(SoundEvents.ITEM_BREAK),
+                            SoundSource.PLAYERS, owner.getX(), owner.getY(), owner.getZ(), 0.7f, 0.5f,
+                            owner.level().random.nextLong()));
                 }
             }
             // if headshots are disabled, still use the regular multiplier
@@ -101,21 +100,25 @@ public abstract class AbstractArrowMixin extends Entity {
                 }
             }
 
-            baseDamage *= multiplier;
+            this.baseDamage *= multiplier;
         }
     }
 
+    /**
+     * checks if the hit was a headshot, and returns the hit position, if it was, null otherwise
+     * @param hit hit result of the original hit
+     * @return hit position on the head hit box, if there is one, {@code null} otherwise
+     */
     @Unique
-    // checks if the hit was a headshot, and returns the hit position, if it was, null otherwise
     private Vec3 vivecraft$isHeadshot(EntityHitResult hit) {
         AABB headBox;
         if ((headBox = Utils.getEntityHeadHitbox(hit.getEntity(), 0.3)) != null) {
-            Vec3 originalHitpos = hit.getEntity()
+            Vec3 originalHitPos = hit.getEntity()
                 .getBoundingBox()
                 .clip(this.position(), this.position().add(this.getDeltaMovement().scale(2.0)))
                 .orElse(this.position().add(this.getDeltaMovement()));
             return headBox
-                .clip(this.position(), originalHitpos)
+                .clip(this.position(), originalHitPos)
                 .orElse(headBox.contains(this.position()) ? this.position() : null);
         }
         return null;
--- common/src/main/java/org/vivecraft/mixin/world/entity/projectile/AbstractHurtingProjectileMixin.java
+++ common/src/main/java/org/vivecraft/mixin/world/entity/projectile/AbstractHurtingProjectileMixin.java
@@ -1,26 +1,27 @@
 package org.vivecraft.mixin.world.entity.projectile;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.projectile.AbstractHurtingProjectile;
 import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.vivecraft.server.ServerVRPlayers;
 import org.vivecraft.server.ServerVivePlayer;
 
 @Mixin(AbstractHurtingProjectile.class)
-public abstract class AbstractHurtingProjectileMixin {
+public class AbstractHurtingProjectileMixin {
 
-    @Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/Entity;getLookAngle()Lnet/minecraft/world/phys/Vec3;"), method = "hurt(Lnet/minecraft/world/damagesource/DamageSource;F)Z")
-    public Vec3 vivecraft$hurtvive(Entity instance) {
+    @WrapOperation(method = "hurt", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/Entity;getLookAngle()Lnet/minecraft/world/phys/Vec3;"))
+    private Vec3 vivecraft$deflect(Entity instance, Operation<Vec3> original) {
         if (instance instanceof ServerPlayer player) {
             ServerVivePlayer serverviveplayer = ServerVRPlayers.getVivePlayer(player);
             if (serverviveplayer != null && serverviveplayer.isVR()) {
                 return serverviveplayer.getHMDDir();
             }
         }
-        return instance.getLookAngle();
+        return original.call(instance);
     }
 }
