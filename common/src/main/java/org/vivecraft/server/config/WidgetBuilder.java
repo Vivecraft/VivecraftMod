647de112a8cddcdb9afff28eb1f9bdf953a3495c
fayer3 <bayer.florian@aon.at>
2024-10-04 19:26:16 +0200

    java doc for WidgetBuilder

--- common/src/main/java/org/vivecraft/server/config/WidgetBuilder.java
+++ common/src/main/java/org/vivecraft/server/config/WidgetBuilder.java
@@ -8,15 +8,28 @@ import org.vivecraft.client.gui.settings.GuiListValueEditScreen;
 import java.util.function.Supplier;
 
 public class WidgetBuilder {
+    /**
+     * creates a simple ConfigValue Button that does nothing
+     * @param value ConfigValue for this button
+     * @param width width of the button
+     * @param height height of the button
+     * @return Button with the value as text, and the comment as tooltip
+     */
     public static Supplier<AbstractWidget> getBaseWidget(ConfigBuilder.ConfigValue<?> value, int width, int height) {
         return () -> Button
-            .builder(Component.literal("" + value.get()), button -> {
-            })
+            .builder(Component.literal("" + value.get()), button -> {})
             .bounds(0, 0, width, height)
             .tooltip(Tooltip.create(Component.literal(value.getComment())))
             .build();
     }
 
+    /**
+     * creates a Button that toggles the BooleanValue
+     * @param booleanValue BooleanValue for this button
+     * @param width width of the button
+     * @param height height of the button
+     * @return Button with the value as text, and the comment as tooltip
+     */
     public static Supplier<AbstractWidget> getOnOffWidget(ConfigBuilder.BooleanValue booleanValue, int width, int height) {
         return () -> CycleButton
             .onOffBuilder(booleanValue.get())
@@ -25,19 +38,27 @@ public class WidgetBuilder {
             .create(0, 0, width, height, Component.empty(), (button, bool) -> booleanValue.set(bool));
     }
 
+    /**
+     * creates an EditBox that holds the StringValue
+     * any changes to the EditBox Aare saved in the StringValue
+     * @param stringValue StringValue for this editbox
+     * @param width width of the editbox
+     * @param height height of the editbox
+     * @return EditBox with the value as text, and the comment as tooltip
+     */
     public static Supplier<AbstractWidget> getEditBoxWidget(ConfigBuilder.StringValue stringValue, int width, int height) {
         return () -> {
             EditBox box = new EditBox(Minecraft.getInstance().font, 0, 0, width - 1, height, Component.literal(stringValue.get())) {
                 @Override
-                public boolean charTyped(char c, int i) {
-                    boolean ret = super.charTyped(c, i);
+                public boolean charTyped(char character, int modifiers) {
+                    boolean ret = super.charTyped(character, modifiers);
                     stringValue.set(this.getValue());
                     return ret;
                 }
 
                 @Override
-                public boolean keyPressed(int i, int j, int k) {
-                    boolean ret = super.keyPressed(i, j, k);
+                public boolean keyPressed(int keyCode, int scanCode, int modifiers) {
+                    boolean ret = super.keyPressed(keyCode, scanCode, modifiers);
                     stringValue.set(this.getValue());
                     return ret;
                 }
@@ -49,6 +70,13 @@ public class WidgetBuilder {
         };
     }
 
+    /**
+     * creates a Button that cycles through the values of the InListValue
+     * @param inListValue InListValue for this button
+     * @param width width of the button
+     * @param height height of the button
+     * @return Button with the value as text, and the comment as tooltip
+     */
     public static <T> Supplier<AbstractWidget> getCycleWidget(ConfigBuilder.InListValue<T> inListValue, int width, int height) {
         return () -> CycleButton
             .builder((newValue) -> Component.literal("" + newValue))
@@ -60,6 +88,13 @@ public class WidgetBuilder {
             .create(0, 0, width, height, Component.empty(), (button, newValue) -> inListValue.set((T) newValue));
     }
 
+    /**
+     * creates a Slider that holds the NumberValue
+     * @param numberValue NumberValue for this slider
+     * @param width width of the slider
+     * @param height height of the slider
+     * @return Slider with the range of the numberValue, and the comment as tooltip
+     */
     public static <E extends Number> Supplier<AbstractWidget> getSliderWidget(ConfigBuilder.NumberValue<E> numberValue, int width, int height) {
         return () -> {
             AbstractSliderButton widget = new AbstractSliderButton(0, 0, width, height, Component.literal("" + numberValue.get()), numberValue.normalize()) {
@@ -78,15 +113,22 @@ public class WidgetBuilder {
         };
     }
 
+    /**
+     * creates a Button that opens an edit Screen for the given ListValue
+     * @param listValue ListValue for this button
+     * @param width width of the button
+     * @param height height of the button
+     * @return Button that opens a screen to edit the list of {@code listValue}, and the comment as tooltip
+     */
     public static <T> Supplier<AbstractWidget> getEditListWidget(ConfigBuilder.ListValue<T> listValue, int width, int height) {
         // TODO handle other types than String
         return () -> Button
             .builder(
                 Component.translatable("vivecraft.options.editlist"),
-                button -> Minecraft.getInstance()
-                    .setScreen(
-                        new GuiListValueEditScreen(Component.literal(listValue.getPath().substring(listValue.getPath().lastIndexOf("."))), Minecraft.getInstance().screen, (ConfigBuilder.ListValue<String>) listValue)
-                    ))
+                button -> Minecraft.getInstance().setScreen(
+                    new GuiListValueEditScreen(
+                        Component.literal(listValue.getPath().substring(listValue.getPath().lastIndexOf("."))),
+                        Minecraft.getInstance().screen, (ConfigBuilder.ListValue<String>) listValue)))
             .size(width, height)
             .tooltip(Tooltip.create(Component.literal(listValue.getComment())))
             .build();
