52073324a8e720bd3294352ac67a288f27ebdda0
hammy275 <hammy275@gmail.com>
2025-06-13 12:27:34 -0400

    Remove Unused Import

--- common/src/main/java/org/vivecraft/api/VRAPI.java
+++ common/src/main/java/org/vivecraft/api/VRAPI.java
@@ -1,7 +1,6 @@
 package org.vivecraft.api;
 
 import net.minecraft.world.entity.player.Player;
-import org.vivecraft.api.client.VRClientAPI;
 import org.vivecraft.api.data.VRPoseHistory;
 import org.vivecraft.api.data.VRPose;
 import org.vivecraft.common.api_impl.VRAPIImpl;
