a2128e534efea2985a0ea646ea47efd76da3028e
fayer3 <bayer.florian@aon.at>
2025-06-15 17:52:19 +0200

    remove beta qualifier, and clarify that the input for setupRenderingAtHand is expected to be an identity

--- common/src/main/java/org/vivecraft/api/client/VRRenderingAPI.java
+++ common/src/main/java/org/vivecraft/api/client/VRRenderingAPI.java
@@ -1,10 +1,10 @@
 package org.vivecraft.api.client;
 
-import com.google.common.annotations.Beta;
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.phys.Vec3;
 import org.joml.Matrix4f;
+import org.vivecraft.api.data.VRPose;
 import org.vivecraft.client.api_impl.VRRenderingAPIImpl;
 import org.vivecraft.client_vr.render.RenderPass;
 
@@ -44,35 +44,32 @@ public interface VRRenderingAPI {
 
     /**
      * Gets the position that the provided {@link InteractionHand} renders at. Unlike
-     * {@link org.vivecraft.api.data.VRPose#getHand(InteractionHand)} from {@link VRClientAPI#getWorldRenderPose()},
+     * {@link VRPose#getHand(InteractionHand)} from {@link VRClientAPI#getWorldRenderPose()},
      * this returns a reasonable, default value for seated mode.
      *
      * @param hand The hand to get the rendering position of.
      * @return The rendering position for the provided hand.
      * @since 1.3.0
      */
-    @Beta
     Vec3 getHandRenderPos(InteractionHand hand);
 
     /**
      * Sets the provided {@link PoseStack} to render at the position of and with the rotation of the provided
-     * {@link InteractionHand}.
+     * {@link InteractionHand}, this assumes the given {@code stack} to be set to an identity.
      *
      * @param hand  The hand to set the PoseStack to.
      * @param stack The PoseStack to be set.
      * @since 1.3.0
      */
-    @Beta
     void setupRenderingAtHand(InteractionHand hand, PoseStack stack);
 
     /**
      * Sets the provided {@link Matrix4f} to render at the position of and with the rotation of the provided
-     * {@link InteractionHand}.
+     * {@link InteractionHand}, this assumes the given {@code matrix} to be an identity.
      *
      * @param hand   The hand to set the PoseStack to.
      * @param matrix The PoseStack to be set.
      * @since 1.3.0
      */
-    @Beta
     void setupRenderingAtHand(InteractionHand hand, Matrix4f matrix);
 }
