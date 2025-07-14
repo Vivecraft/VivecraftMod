d541323ad95b5b1f2bbc00e93312b11ab1eb9239
fayer3 <bayer.florian@aon.at>
2025-07-14 11:27:07 +0200

    import cleanup

--- neoforge/src/main/java/org/vivecraft/neoforge/event/ClientGameEvents.java
+++ neoforge/src/main/java/org/vivecraft/neoforge/event/ClientGameEvents.java
@@ -2,17 +2,11 @@ package org.vivecraft.neoforge.event;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.commands.Commands;
-import net.minecraft.resources.ResourceLocation;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.EventBusSubscriber;
-import net.neoforged.fml.event.lifecycle.FMLConstructModEvent;
-import net.neoforged.neoforge.client.event.AddClientReloadListenersEvent;
 import net.neoforged.neoforge.client.event.RegisterClientCommandsEvent;
-import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 import org.vivecraft.client.gui.settings.VivecraftMainSettings;
-import org.vivecraft.client_vr.ReloadListener;
 import org.vivecraft.neoforge.Vivecraft;
 
 @EventBusSubscriber(value = Dist.CLIENT, bus = EventBusSubscriber.Bus.GAME, modid = Vivecraft.MODID)
