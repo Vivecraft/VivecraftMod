19305ea0bd47ce46a4c26ad90f69d12fb3125d22
SuperPou <superpou1filip@gmail.com>
2025-08-06 20:07:54 +0200

    add Windows Mixed Reality controller openxr bindings

--- common/src/main/java/org/vivecraft/client_vr/provider/openxr/XRBindings.java
+++ common/src/main/java/org/vivecraft/client_vr/provider/openxr/XRBindings.java
@@ -22,6 +22,7 @@ public class XRBindings {
         set.add("/interaction_profiles/oculus/touch_controller");
         set.add("/interaction_profiles/htc/vive_controller");
         set.add("/interaction_profiles/valve/index_controller");
+        set.add("/interaction_profiles/microsoft/motion_controller");
         return set;
     }
 
@@ -233,6 +234,47 @@ public class XRBindings {
         return set;
     }
 
+    private static HashSet<Pair<String, String>> wmrBindings() {
+        HashSet<Pair<String, String>> set = new HashSet<>();
+
+        set.add(new MutablePair<>("/actions/global/in/vivecraft.key.ingameMenuButton", "/user/hand/left/input/menu/click"));
+        set.add(new MutablePair<>("/actions/global/in/key.inventory", "/user/hand/left/input/trackpad/click"));
+
+        set.add(new MutablePair<>("/actions/gui/in/vivecraft.key.guiShift", "/user/hand/left/input/squeeze/click"));
+        set.add(new MutablePair<>("/actions/gui/in/vivecraft.key.guiMiddleClick", "/user/hand/right/input/squeeze/click"));
+        set.add(new MutablePair<>("/actions/gui/in/vivecraft.key.guiLeftClick", "/user/hand/right/input/trigger/value"));
+        set.add(new MutablePair<>("/actions/gui/in/vivecraft.key.guiRightClick", "/user/hand/right/input/trackpad/click"));
+        set.add(new MutablePair<>("/actions/gui/in/vivecraft.key.guiScrollAxis", "/user/hand/right/input/thumbstick/y"));
+
+        set.add(new MutablePair<>("/actions/ingame/in/vivecraft.key.hotbarPrev", "/user/hand/left/input/squeeze/click"));
+        set.add(new MutablePair<>("/actions/ingame/in/vivecraft.key.hotbarNext", "/user/hand/right/input/squeeze/click"));
+        set.add(new MutablePair<>("/actions/ingame/in/key.attack", "/user/hand/right/input/trigger/value"));
+        set.add(new MutablePair<>("/actions/ingame/in/vivecraft.key.teleport", "/user/hand/left/input/trigger/value"));
+        set.add(new MutablePair<>("/actions/ingame/in/vivecraft.key.radialMenu", "/user/hand/right/input/menu/click"));
+        set.add(new MutablePair<>("/actions/ingame/in/key.use", "/user/hand/right/input/trackpad/click"));
+        set.add(new MutablePair<>("/actions/ingame/in/vivecraft.key.teleportFallback", "/user/hand/left/input/trigger/value"));
+        set.add(new MutablePair<>("/actions/ingame/in/key.jump", "/user/hand/right/input/thumbstick/click"));
+        set.add(new MutablePair<>("/actions/ingame/in/vivecraft.key.freeMoveStrafe", "/user/hand/left/input/thumbstick"));
+        set.add(new MutablePair<>("/actions/ingame/in/key.sneak", "/user/hand/left/input/thumbstick/click"));
+        set.add(new MutablePair<>("/actions/ingame/in/vivecraft.key.rotateAxis", "/user/hand/right/input/thumbstick"));
+
+        set.add(new MutablePair<>("/actions/keyboard/in/vivecraft.key.keyboardShift", "/user/hand/left/input/squeeze/click"));
+        set.add(new MutablePair<>("/actions/keyboard/in/vivecraft.key.keyboardShift", "/user/hand/right/input/squeeze/click"));
+        set.add(new MutablePair<>("/actions/keyboard/in/vivecraft.key.keyboardClick", "/user/hand/left/input/trigger/value"));
+        set.add(new MutablePair<>("/actions/keyboard/in/vivecraft.key.keyboardClick", "/user/hand/right/input/trigger/value"));
+
+        set.add(new MutablePair<>("/actions/contextual/in/vivecraft.key.vrInteract", "/user/hand/left/input/squeeze/click"));
+        set.add(new MutablePair<>("/actions/contextual/in/vivecraft.key.vrInteract", "/user/hand/right/input/squeeze/click"));
+        set.add(new MutablePair<>("/actions/contextual/in/vivecraft.key.vrInteract", "/user/hand/left/input/trigger/value"));
+        set.add(new MutablePair<>("/actions/contextual/in/vivecraft.key.vrInteract", "/user/hand/right/input/trigger/value"));
+
+        set.add(new MutablePair<>("/actions/contextual/in/vivecraft.key.climbeyGrab", "/user/hand/left/input/squeeze/click"));
+        set.add(new MutablePair<>("/actions/contextual/in/vivecraft.key.climbeyGrab", "/user/hand/right/input/squeeze/click"));
+        set.add(new MutablePair<>("/actions/contextual/in/vivecraft.key.climbeyGrab", "/user/hand/left/input/trigger/value"));
+        set.add(new MutablePair<>("/actions/contextual/in/vivecraft.key.climbeyGrab", "/user/hand/right/input/trigger/value"));
+        return set;
+    }
+
     private static HashSet<Pair<String, String>> defaultBindings() {
         HashSet<Pair<String, String>> set = new HashSet<>();
 
@@ -272,6 +314,9 @@ public class XRBindings {
             case "/interaction_profiles/bytedance/pico4_controller",
                  "/interaction_profiles/bytedance/pico_neo3_controller" -> {
                 return picoBindings();
+            }
+            case "/interaction_profiles/microsoft/motion_controller" -> {
+                return wmrBindings();
             }
             default -> {
                 return defaultBindings();
