b438b5175f9fea6e675713b5346d8306b2d3bfcf
fayer3 <bayer.florian@aon.at>
2025-10-04 13:02:26 +0200

    remove some unused extends from mixins

--- common/src/main/java/org/vivecraft/mixin/world/entity/monster/EndermanMixin.java
+++ common/src/main/java/org/vivecraft/mixin/world/entity/monster/EndermanMixin.java
@@ -3,21 +3,14 @@ package org.vivecraft.mixin.world.entity.monster;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.monster.EnderMan;
-import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.vivecraft.server.ServerVRPlayers;
 
 @Mixin(EnderMan.class)
-public abstract class EndermanMixin extends Monster {
-
-    protected EndermanMixin(EntityType<? extends Monster> entityType, Level level) {
-        super(entityType, level);
-    }
+public abstract class EndermanMixin {
 
     @ModifyExpressionValue(method = "isBeingStaredBy", at = @At(value = "CONSTANT", args = "doubleValue=0.025"))
     private double vivecraft$biggerViewCone(double original, @Local(argsOnly = true) Player player) {
