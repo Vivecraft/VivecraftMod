3afb1260dfafa7eed01be3e6379957151eb6f58d
fayer3 <bayer.florian@aon.at>
2025-11-02 16:06:46 +0100

    fix crash neoforge 21.10.24+, and bump min version to that

--- neoforge/src/main/java/org/vivecraft/neoforge/XeventsImpl.java
+++ neoforge/src/main/java/org/vivecraft/neoforge/XeventsImpl.java
@@ -1,6 +1,7 @@
 package org.vivecraft.neoforge;
 
 import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.Minecraft;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.block.state.BlockState;
@@ -14,14 +15,17 @@ public class XeventsImpl implements Xevents {
         Player player, PoseStack poseStack, BlockState blockState, BlockPos blockPos)
     {
         return ClientHooks.renderBlockOverlay(player, poseStack, RenderBlockScreenEffectEvent.OverlayType.BLOCK,
-            blockState, blockPos);
+            blockState, blockPos, Minecraft.getInstance().getAtlasManager(),
+            Minecraft.getInstance().renderBuffers().bufferSource());
     }
 
     public static boolean renderWaterOverlay(Player player, PoseStack poseStack) {
-        return ClientHooks.renderWaterOverlay(player, poseStack);
+        return ClientHooks.renderWaterOverlay(player, poseStack, Minecraft.getInstance().getAtlasManager(),
+            Minecraft.getInstance().renderBuffers().bufferSource());
     }
 
     public static boolean renderFireOverlay(Player player, PoseStack poseStack) {
-        return ClientHooks.renderFireOverlay(player, poseStack);
+        return ClientHooks.renderFireOverlay(player, poseStack, Minecraft.getInstance().getAtlasManager(),
+            Minecraft.getInstance().renderBuffers().bufferSource());
     }
 }
