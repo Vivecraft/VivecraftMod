1cf8384c2a4aeacfd973913ee5c16af0c92dc338
fayer3 <bayer.florian@aon.at>
2025-11-12 21:35:45 +0100

    fix crash when opening the quick commands gui in nonvr

--- common/src/main/java/org/vivecraft/client/gui/settings/GuiQuickCommandsInGame.java
+++ common/src/main/java/org/vivecraft/client/gui/settings/GuiQuickCommandsInGame.java
@@ -48,7 +48,6 @@ public class GuiQuickCommandsInGame extends Screen {
 
     @Override
     public void render(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick) {
-        this.renderBackground(guiGraphics, mouseX, mouseY, partialTick);
         guiGraphics.drawCenteredString(this.font, this.getTitle(), this.width / 2, 16, 0xFFFFFFFF);
         super.render(guiGraphics, mouseX, mouseY, partialTick);
     }
