1e2d551f4a81e1f33f7b546f657f2ca9b7a18e2f
fayer3 <bayer.florian@aon.at>
2025-11-30 01:44:09 +0100

    fix roomscale bow notch animation, not sure why I removed this in the backport

---
+++ common/src/main/java/org/vivecraft/mixin/client_vr/renderer/ItemPropertiesVRMixin.java
@@ -0,0 +1,30 @@
+package org.vivecraft.mixin.client_vr.renderer;
+
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.item.ItemProperties;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.vivecraft.client_vr.ClientDataHolderVR;
+import org.vivecraft.client_vr.VRState;
+import org.vivecraft.client_vr.gameplay.trackers.BowTracker;
+
+@Mixin(ItemProperties.class)
+public class ItemPropertiesVRMixin {
+
+    @ModifyReturnValue(method = "method_27889", at = @At("RETURN"))
+    private static float vivecraft$roomscaleBowNotch(
+        float original, @Local(argsOnly = true) ItemStack itemStack, @Local(argsOnly = true) LivingEntity livingEntity)
+    {
+        if (VRState.VR_RUNNING && livingEntity == Minecraft.getInstance().player && BowTracker.isBow(itemStack) &&
+            ClientDataHolderVR.getInstance().bowTracker.isNotched())
+        {
+            return 1F;
+        } else {
+            return original;
+        }
+    }
+}
