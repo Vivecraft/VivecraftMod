fd36590b818c3130444c591a8ed1b4373cca440f
hammy275 <hammy275@gmail.com>
2025-11-30 21:31:13 -0500

    Nullable Fixes

--- common/src/main/java/org/vivecraft/client/api_impl/VRRenderingAPIImpl.java
+++ common/src/main/java/org/vivecraft/client/api_impl/VRRenderingAPIImpl.java
@@ -5,7 +5,6 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.phys.Vec3;
-import org.jetbrains.annotations.Nullable;
 import org.joml.Matrix4f;
 import org.vivecraft.api.client.VRRenderingAPI;
 import org.vivecraft.api.client.data.RenderPass;
@@ -15,6 +14,8 @@ import org.vivecraft.client_vr.ClientDataHolderVR;
 import org.vivecraft.client_vr.VRState;
 import org.vivecraft.client_vr.render.helpers.RenderHelper;
 import org.vivecraft.client_xr.render_pass.RenderPassType;
+
+import javax.annotation.Nullable;
 
 public class VRRenderingAPIImpl implements VRRenderingAPI {
 
@@ -63,7 +64,8 @@ public class VRRenderingAPIImpl implements VRRenderingAPI {
     }
 
     @Override
-    public @Nullable VRPose getWorldRenderPose(Player player) {
+    @Nullable
+    public VRPose getWorldRenderPose(Player player) {
         if (player.isLocalPlayer()) {
             return VRClientAPIImpl.INSTANCE.getWorldRenderPose();
         } else {
