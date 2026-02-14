410300ca648dd82622fa2fd60f892abcf4b987c6
fayer3 <bayer.florian@aon.at>
2026-02-14 22:52:42 +0100

    1.21.1 fixes

--- common/src/main/java/org/vivecraft/mixin/client_vr/world/ItemVRMixin.java
+++ common/src/main/java/org/vivecraft/mixin/client_vr/world/ItemVRMixin.java
@@ -6,12 +6,13 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.vivecraft.data.ViveItems;
 
 @Mixin(Item.class)
 public class ItemVRMixin {
 
     @ModifyExpressionValue(method = "use", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/player/Player;canEat(Z)Z"))
     private boolean vivecraft$alwaysAllowEasterEggEating(boolean canEat, @Local ItemStack itemStack) {
-        return canEat || itemStack.getHoverName().getString().equals("EAT ME");
+        return canEat || ViveItems.isGrowPie(itemStack);
     }
 }
