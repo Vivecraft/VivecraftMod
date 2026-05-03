e37197a8d3e40094d21b37f7e838e516f6116c0d
fayer3 <bayer.florian@aon.at>
2026-05-03 17:48:54 +0200

    fix iris vivecraft camera uniform

--- common/src/main/java/org/vivecraft/mod_compat_vr/iris/mixin/IrisCameraPositionTrackerMixin.java
+++ common/src/main/java/org/vivecraft/mod_compat_vr/iris/mixin/IrisCameraPositionTrackerMixin.java
@@ -1,6 +1,6 @@
 package org.vivecraft.mod_compat_vr.iris.mixin;
 
-import org.joml.Vector3d;
+import net.coderbot.iris.vendored.joml.Vector3d;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.Shadow;
---
+++ stubs/src/main/java/net/coderbot/iris/vendored/joml/Vector3d.java
@@ -0,0 +1,7 @@
+package net.coderbot.iris.vendored.joml;
+
+public class Vector3d {
+    public double x;
+    public double y;
+    public double z;
+}
