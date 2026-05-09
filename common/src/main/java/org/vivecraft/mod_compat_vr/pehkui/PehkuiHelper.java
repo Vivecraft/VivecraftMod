544ca69ce92bbb4dca0ede7786d802c86f15c822
fayer3 <bayer.florian@aon.at>
2026-05-09 15:45:02 +0200

    fix pehkui compat

--- common/src/main/java/org/vivecraft/mod_compat_vr/pehkui/PehkuiHelper.java
+++ common/src/main/java/org/vivecraft/mod_compat_vr/pehkui/PehkuiHelper.java
@@ -2,8 +2,18 @@ package org.vivecraft.mod_compat_vr.pehkui;
 
 import net.minecraft.world.entity.Entity;
 import org.vivecraft.Xloader;
+import org.vivecraft.client_vr.settings.VRSettings;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class PehkuiHelper {
+
+    private static boolean INITIALIZED = false;
+    private static boolean INIT_FAILED = false;
+
+    private static Method ScaleUtils_getEyeHeightScale;
+    private static Method ScaleUtils_getBoundingBoxHeightScale;
 
     public static boolean isLoaded() {
         return Xloader.INSTANCE.isModLoaded("pehkui");
@@ -17,8 +27,14 @@ public class PehkuiHelper {
      * @return scale of the entities eye height
      */
     public static float getEntityEyeHeightScale(Entity entity, float partialTick) {
-        // TODO 26.1 use reflection
-        return 1;//ScaleUtils.getEyeHeightScale(entity, partialTick);
+        if (init()) {
+            try {
+                return (float) ScaleUtils_getEyeHeightScale.invoke(null, entity, partialTick);
+            } catch (InvocationTargetException | IllegalAccessException e) {
+                VRSettings.LOGGER.error("Vivecraft: couldn't get pehkui entity eye height scale:", e);
+            }
+        }
+        return 1F;
     }
 
     /**
@@ -29,7 +45,36 @@ public class PehkuiHelper {
      * @return scale of the entities bounding box
      */
     public static float getEntityBbScale(Entity entity, float partialTick) {
-        // TODO 26.1 use reflection
-        return 1;//ScaleUtils.getBoundingBoxHeightScale(entity, partialTick);
+        if (init()) {
+            try {
+                return (float) ScaleUtils_getBoundingBoxHeightScale.invoke(null, entity, partialTick);
+            } catch (InvocationTargetException | IllegalAccessException e) {
+                VRSettings.LOGGER.error("Vivecraft: couldn't get pehkui entity bb scale:", e);
+            }
+        }
+        return 1F;
+    }
+
+    /**
+     * initializes all Reflections
+     *
+     * @return if init was successful
+     */
+    private static boolean init() {
+        if (INITIALIZED) {
+            // try to softly fail when something went wrong
+            return !INIT_FAILED;
+        }
+        try {
+            Class<?> ScaleUtils = Class.forName("virtuoel.pehkui.util.ScaleUtils");
+            ScaleUtils_getEyeHeightScale = ScaleUtils.getDeclaredMethod("getEyeHeightScale", Entity.class, float.class);
+            ScaleUtils_getBoundingBoxHeightScale = ScaleUtils.getDeclaredMethod("getBoundingBoxHeightScale",
+                Entity.class, float.class);
+        } catch (ClassNotFoundException | NoSuchMethodException e) {
+            INIT_FAILED = true;
+            VRSettings.LOGGER.error("Vivecraft: Failed to initialize Pehkui compat:", e);
+        }
+        INITIALIZED = true;
+        return !INIT_FAILED;
     }
 }
