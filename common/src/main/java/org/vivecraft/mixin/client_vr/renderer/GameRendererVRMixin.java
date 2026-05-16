43a6587d1d0aa8ad06dd5b5ba0a72bf351684675
fayer3 <bayer.florian@aon.at>
2026-05-17 01:15:43 +0200

    actually update the undistorted projection matrix

--- common/src/main/java/org/vivecraft/mixin/client_vr/renderer/GameRendererVRMixin.java
+++ common/src/main/java/org/vivecraft/mixin/client_vr/renderer/GameRendererVRMixin.java
@@ -55,6 +55,7 @@ import org.vivecraft.client_vr.VRState;
 import org.vivecraft.client_vr.extensions.GameRendererExtension;
 import org.vivecraft.client_vr.extensions.OptionInstanceExtension;
 import org.vivecraft.client_vr.extensions.WindowExtension;
+import org.vivecraft.client_vr.render.VRShaders;
 import org.vivecraft.client_vr.render.XRCamera;
 import org.vivecraft.client_vr.render.helpers.RenderHelper;
 import org.vivecraft.client_vr.render.helpers.VREffectsHelper;
@@ -388,6 +389,14 @@ public abstract class GameRendererVRMixin
         this.vivecraft$cacheRVEPos(this.minecraft.getCameraEntity());
         this.vivecraft$setupRVE();
         this.vivecraft$setupOverlayStatus();
+    }
+
+    @ModifyExpressionValue(method = "renderLevel", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/GameRenderer;getProjectionMatrix(F)Lorg/joml/Matrix4f;"))
+    private Matrix4f vivecraft$captureUndistortedProjection(Matrix4f original) {
+        if (VRState.VR_RUNNING) {
+            VRShaders.setUndistortedProj(original);
+        }
+        return original;
     }
 
     @ModifyArg(method = "renderLevel", at = @At(value = "INVOKE", target = "Lorg/joml/Matrix4f;rotate(FLorg/joml/Vector3fc;)Lorg/joml/Matrix4f;", remap = false), index = 0, remap = true)
