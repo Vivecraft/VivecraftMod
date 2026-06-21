57ed21d651c4411ece9a205da845af7e65c034f9
fayer3 <bayer.florian@aon.at>
2026-06-21 19:43:45 +0200

    1.21.11 optifine fix

--- common/src/main/java/org/vivecraft/mod_compat_vr/optifine/mixin/ShadersRenderVRMixin.java
+++ common/src/main/java/org/vivecraft/mod_compat_vr/optifine/mixin/ShadersRenderVRMixin.java
@@ -4,7 +4,7 @@ import com.llamalad7.mixinextras.sugar.Local;
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.Camera;
-import net.minecraft.client.DeltaTracker;
+import net.minecraft.client.Minecraft;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.Shadow;
@@ -21,7 +21,7 @@ import org.vivecraft.mod_compat_vr.shaders.ShadersHelper;
 public class ShadersRenderVRMixin {
 
     @Shadow(remap = false)
-    public static void updateActiveRenderInfo(Camera activeRenderInfo, DeltaTracker deltaTracker) {}
+    public static void updateActiveRenderInfo(Camera activeRenderInfo, Minecraft mc, float partialTick) {}
 
     @Inject(method = {"renderHandTranslucent", "renderHand0"}, at = @At("HEAD"), remap = false, cancellable = true)
     private static void vivecraft$noTranslucentHandsInVR(CallbackInfo ci) {
@@ -39,14 +39,13 @@ public class ShadersRenderVRMixin {
 
     @Inject(method = "renderShadowMap", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/Minecraft;getInstance()Lnet/minecraft/client/Minecraft;", remap = true), remap = false, cancellable = true)
     private static void vivecraft$shadowsOnlyOnce(
-        CallbackInfo ci, @Local(argsOnly = true) Camera activeRenderInfo,
-        @Local(argsOnly = true) DeltaTracker deltaTracker, @Local(argsOnly = true) float partialTick)
+        CallbackInfo ci, @Local(argsOnly = true) Camera activeRenderInfo, @Local(argsOnly = true) float partialTick)
     {
         if (!RenderPassType.isVanilla() && !ClientDataHolderVR.getInstance().isFirstPass &&
             !ShadersHelper.isSlowMode())
         {
             RenderSystem.backupProjectionMatrix();
-            updateActiveRenderInfo(activeRenderInfo, deltaTracker);
+            updateActiveRenderInfo(activeRenderInfo, Minecraft.getInstance(), partialTick);
             OptifineHelper.setCameraShadow(new PoseStack(), activeRenderInfo, partialTick);
             RenderSystem.restoreProjectionMatrix();
             ci.cancel();
